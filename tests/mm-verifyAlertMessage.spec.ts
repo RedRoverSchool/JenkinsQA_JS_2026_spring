8c604502aaa2fdc56983f561145c773eaddaf387
BorisS <teachandlearn999@gmail.com>
2026-04-01 00:42:26 -0700

    fix ci

--- tests/mm-verifyAlertMessage.spec.ts
+++ tests/mm-verifyAlertMessage.spec.ts
@@ -2,11 +2,11 @@ import { expect } from "@playwright/test";
 import { test } from "@/base";
 import { jenkinsData } from "@/tests/testData/mm-jenkinsData";
 
-test.describe("US_03.006 | Pipeline Configuration > Save or Apply>", () => {
+test.describe.only("US_03.006 | Pipeline Configuration > Save or Apply>", () => {
 	test("TC_03.006.02 |  Verify Alert Message for Unsaved Changes on Navigation", async ({ page }) => {
 		await page.locator("#side-panel a[href$='newJob']").click();
 		await page.locator("#name").fill(jenkinsData.projectCategory.pipeline);
-		await page.getByLabel('Pipeline', { exact: true }).click();
+		await page.locator("li.org_jenkinsci_plugins_workflow_job_WorkflowJob").click();
 		await page.locator("button#ok-button").click();
 		await page.locator("textarea[name='description']").fill(jenkinsData.configureCategory.description);
 		page.on("dialog", async (dialog) => {
