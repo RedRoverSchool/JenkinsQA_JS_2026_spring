be6be301d67c0ef541f0ad6ded6609540ea927cc
BorisS <teachandlearn999@gmail.com>
2026-03-31 23:39:45 -0700

    fix ci

--- tests/mm-verifyAlertMessage.spec.ts
+++ tests/mm-verifyAlertMessage.spec.ts
@@ -6,7 +6,7 @@ test.describe("US_03.006 | Pipeline Configuration > Save or Apply>", () => {
 	test("TC_03.006.02 |  Verify Alert Message for Unsaved Changes on Navigation", async ({ page }) => {
 		await page.locator("#side-panel a[href$='newJob']").click();
 		await page.locator("#name").fill(jenkinsData.projectCategory.pipeline);
-		await page.getByRole("radio", { name: "Pipeline" }).click();
+		await page.getByRole("radio", { name: "Pipeline", exact: true }).click();
 		await page.locator("button#ok-button").click();
 		await page.locator("textarea[name='description']").fill(jenkinsData.configureCategory.description);
 		page.on("dialog", async (dialog) => {
