d489d8f765ca21d62128563a1005cdfc4fb85cfa
BorisS <teachandlearn999@gmail.com>
2026-04-01 09:33:24 -0700

    fix ci

--- tests/pero-verifyItemTypeSelectionSectionIsVisible.spec.ts
+++ tests/pero-verifyItemTypeSelectionSectionIsVisible.spec.ts
@@ -5,8 +5,8 @@ import { jenkinsData } from "@/tests/testData/bs-jenkinsData";
 test.describe.only("US_01.004 | New Item > Select an Item type", () => {
 	test("TC_01.004.04 | Verify item type selection section is visible", async ({ page }) => {
 		const nameItems = [
-			"Freestyle project",
 			"Pipeline",
+			"Freestyle project",
 			"Multi-configuration project",
 			"Folder",
 			"Multibranch Pipeline",
@@ -14,8 +14,8 @@ test.describe.only("US_01.004 | New Item > Select an Item type", () => {
 		];
 
 		const descriptionItems = [
+			"Build, test, and deploy using pipelines. Supports stages, parallel work, and running on multiple agents.",
 			"Classic, general-purpose job type that checks out from up to one SCM, executes build steps serially, followed by post-build steps like archiving artifacts and sending email notifications.",
-			"Orchestrates long-running activities that can span multiple build agents. Suitable for building pipelines (formerly known as workflows) and/or organizing complex activities that do not easily fit in free-style job type.",
 			"Suitable for projects that need a large number of different configurations, such as testing on multiple environments, platform-specific builds, etc.",
 			"Creates a container that stores nested items in it. Useful for grouping things together. Unlike view, which is just a filter, a folder creates a separate namespace, so you can have multiple things of the same name as long as they are in different folders.",
 			"Creates a set of Pipeline projects according to detected branches in one SCM repository.",
@@ -28,7 +28,7 @@ test.describe.only("US_01.004 | New Item > Select an Item type", () => {
 		const textNameItemsOnPage = await page.locator("#items li span.label").allInnerTexts();
 		const elNameItemsOnPage = await page.locator("#items li span.label").all();
 
-		expect(textNameItemsOnPage.toSorted()).toEqual(nameItems.toSorted());
+		expect(textNameItemsOnPage).toEqual(nameItems);
 		for (const nameItemOnPage of elNameItemsOnPage) {
 			await expect(nameItemOnPage).toBeVisible();
 		}
@@ -37,7 +37,7 @@ test.describe.only("US_01.004 | New Item > Select an Item type", () => {
 		console.log("desc: ", textDescriptionItemsOnPage)
         const elDescriptionItemsOnPage = await page.locator("#items li div.desc").all();
 
-		expect(textDescriptionItemsOnPage.toSorted()).toEqual(descriptionItems.toSorted());
+		expect(textDescriptionItemsOnPage).toEqual(descriptionItems);
 		for (const descriptionItemOnPage of elDescriptionItemsOnPage) {
 			await expect(descriptionItemOnPage).toBeVisible();
 		}
