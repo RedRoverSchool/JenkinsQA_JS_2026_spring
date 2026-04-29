059d07033fc135f193b5eb5a636d1214af187773
ok <okepych@yahoo.com>
2026-04-28 22:36:00 -0500

    TC_02.002.01 | Verify Description field is displayed

---
+++ tests/ok-verifyFreestyleProjectDescription.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect, Page } from "@/base";
+import { jobName } from "./testData/ok-data";
+
+test.describe("US_02.002 | Freestyle Project Configuration > Project Description", () => {
+
+    test.beforeEach(async ({ page }: { page: Page }) => {
+        await page.getByRole('link', { name: "Create a job" }).click();
+        await page.getByRole('textbox', { name: "name" }).fill(jobName);
+        await page.getByRole('radio', { name: "Freestyle project" }).click();
+        await page.getByRole('button', { name: "OK" }).click();
+    });
+
+    test("TC_02.002.01 | Verify Description field is displayed", async ({ page }: { page: Page }) => {
+        const sectionTitle = page.locator("#general");
+        await expect(sectionTitle).toBeVisible();
+
+        const descriptionTextarea = page.locator("textarea[name='description']");
+        await expect(descriptionTextarea).toBeVisible();
+    });
+});
