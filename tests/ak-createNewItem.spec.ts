95ff8d943bc64ce3af4707e00570387dba4a0cab
Andrii Koss <sqakross@gmail.com>
2026-04-29 07:06:42 -0700

    update code

--- tests/ak-createNewItem.spec.ts
+++ tests/ak-createNewItem.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect, Page } from "@/base";
 import { jenkinsData } from "./testData/ak-data";
 
-test.describe("US_01.001.02 | New Item > Create a new item > Validate unsupported special characters", () => {
+test.describe("US_01.001| New Item > Create a new item ", () => {
   for (const invalidChar of jenkinsData.unsupportedSpecialCharacters) {
-    test(`TC_01.002.01 | Verify validation error for unsupported character "${invalidChar}"`, async ({ page }: { page: Page }) => {
+    test(`TC_01.002.02 | Verify validation error for unsupported character "${invalidChar}"`, async ({ page }: { page: Page }) => {
       await page.locator("#side-panel a[href$='newJob']").click();
 
       await page.locator("#name").fill(`${jenkinsData.jobName}${invalidChar}job`);
