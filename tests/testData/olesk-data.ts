10cb0ebfbea02a4f863e1fd28ba9a0879905e671
Olesia Klymenko <olesiaklymenko@Olesias-MacBook-Pro.local>
2026-05-01 18:45:56 +0300

    add testData file

---
+++ tests/testData/olesk-data.ts
@@ -0,0 +1,9 @@
+export const folderData = {
+    displayName: "TestFolder",
+    description: "This is test folder description"
+}
+
+export const folderFieldsLocators = {
+    displayName: "input[name= '_.displayNameOrNull']",
+    description: "textarea[name= '_.description']"
+}
