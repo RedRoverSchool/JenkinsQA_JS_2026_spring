3479936c31e54a55c59eb76f87df6787da1f278f
himym1989 <himym1989@gmail.com>
2026-05-02 18:26:57 +0300

    TC_16.008.01 | Verify deleting project via dropdown menu

---
+++ tests/ozh-deletingProject.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect, Page } from '@/base';
+import { ozData } from './testData/ozh-data';
+
+test.describe('US_16.008 | Freestyle Project Management > Delete Project', () => {
+  test.beforeEach(async ({ page }: { page: Page }) => {
+    await page.locator("#side-panel a[href$='newJob']").click();
+    await page.locator('#name').fill(ozData.jobName);
+    await page
+      .locator("#j-add-item-type-standalone-projects ul li[class*='FreeStyleProject']")
+      .click();
+    await page.locator('#ok-button').click();
+    await page.locator('.app-jenkins-logo').click();
+  });
+
+  test('TC_16.008.01 | Verify deleting project via dropdown menu', async ({
+    page,
+  }: {
+    page: Page;
+  }) => {
+    page.on('dialog', async (dialog) => {
+      await dialog.accept();
+    });
+
+    const item = page.locator(`[href*=${ozData.jobName}].jenkins-table__link`);
+    await item.hover();
+    await item.locator('button.jenkins-menu-dropdown-chevron').click();
+    const itemDropDown = page.locator(`div.jenkins-dropdown`);
+    await itemDropDown
+      .locator(`button[href="/job/${ozData.jobName}/doDelete"].jenkins-dropdown__item `)
+      .click();
+
+    await page.click('button[data-id=ok]');
+
+    await expect(page.locator('a[href="newJob"]')).toBeVisible();
+  });
+});
