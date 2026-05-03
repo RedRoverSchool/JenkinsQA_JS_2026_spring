e0b817c23b01d061647aac39f3fd330198def9e5
ZubarewArt <art.zubarev1991@gmail.com>
2026-05-03 20:33:44 +0700

    TC_01.004.11 | New Item > Select an Item type > FreeStyleProject

--- tests/art-Select an Item type.spec.ts
+++ tests/art-Select an Item type.spec.ts
@@ -14,5 +14,18 @@ test.describe("US_01.004 | New Item > Select an Item type", () => {
 
         const job = await page.locator("#projectstatus .jenkins-table__link").getAttribute("href");
         expect(job).toContain(jenkinsData.pipelineName);           
-        });
+    });
+
+     test("TC_01.004.11 | FreeStyleProject", async ({ page }: { page: Page }) => {
+        await page.locator("#side-panel a[href$='newJob']").click();
+        await page.locator("#name").fill(jenkinsData.jobName);
+        await page.locator(".hudson_model_FreeStyleProject").click();
+        await page.locator("#ok-button").click();
+
+        await page.locator(".app-jenkins-logo").click();
+
+        const job = await page.locator("#projectstatus .jenkins-table__link").getAttribute("href");
+        expect(job).toContain(jenkinsData.jobName);           
+    });
+
 });
