78c3d393491de97a732c4ba12791899ce2d2538b
Tatiana Linder <lindertatiana@gmail.com>
2026-05-02 20:57:25 -0700

    Refactor: extract locators and helpers for New Item tests

--- tests/testData/tl-data.ts
+++ tests/testData/tl-data.ts
@@ -1,3 +1,17 @@
-export const invalidItemData = {
-  invalidName: "test?item"
+import { Page } from "@/base";
+
+export const newItemData = {
+  invalidItemName: "test?item",
 };
+
+export const newItemLocators = {
+  newItemLink: "a[href='/view/all/newJob']",
+  itemNameInput: "#name",
+  freestyleProject: "#j-add-item-type-standalone-projects li[class*=FreeStyleProject]",
+  okButton: "#ok-button",
+  invalidNameMessage: "#itemname-invalid",
+};
+
+export async function openNewItemPage(page: Page): Promise<void> {
+  await page.locator(newItemLocators.newItemLink).click();
+}
