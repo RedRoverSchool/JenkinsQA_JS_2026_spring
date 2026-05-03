a0a7325f6788944c5ac51cd69e0b03fea1cfa395
himym1989 <himym1989@gmail.com>
2026-05-03 21:25:38 +0300

    delete before each

--- tests/ozh-deletingProject.spec.ts
+++ tests/ozh-deletingProject.spec.ts
@@ -1,17 +1,7 @@
-import { test, expect, Page } from '@/base';
+import { test, expect, Page } from '../base';
 import { ozData } from './testData/ozh-data';
 
 test.describe('US_16.008 | Freestyle Project Management > Delete Project', () => {
-  test.beforeEach(async ({ page }: { page: Page }) => {
-    await page.locator("#side-panel a[href$='newJob']").click();
-    await page.locator('#name').fill(ozData.jobName);
-    await page
-      .locator("#j-add-item-type-standalone-projects ul li[class*='FreeStyleProject']")
-      .click();
-    await page.locator('#ok-button').click();
-    await page.locator('.app-jenkins-logo').click();
-  });
-
   test('TC_16.008.01 | Verify deleting project via dropdown menu', async ({
     page,
   }: {
@@ -20,7 +10,13 @@ test.describe('US_16.008 | Freestyle Project Management > Delete Project', () =>
     page.on('dialog', async (dialog) => {
       await dialog.accept();
     });
-
+    await page.locator("#side-panel a[href$='newJob']").click();
+    await page.locator('#name').fill(ozData.jobName);
+    await page
+      .locator("#j-add-item-type-standalone-projects ul li[class*='FreeStyleProject']")
+      .click();
+    await page.locator('#ok-button').click();
+    await page.locator('.app-jenkins-logo').click();
     await expect(page.locator(`tr#job_${ozData.jobName}`)).toHaveCount(1);
     const item = page.locator(`[href*=${ozData.jobName}].jenkins-table__link`);
     await item.hover();
