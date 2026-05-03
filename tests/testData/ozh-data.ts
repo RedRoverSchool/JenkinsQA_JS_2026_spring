644f2bb4b06b49242b2e343a86577db2caf8e8d1
himym1989 <himym1989@gmail.com>
2026-05-03 21:19:29 +0300

    TC_02.003.01 |Verify error message when repo URL is invalid

--- tests/testData/ozh-data.ts
+++ tests/testData/ozh-data.ts
@@ -1,4 +1,26 @@
+import { Page } from '../../base';
 export const ozData = {
   jobName: 'TestJob',
   jobNameRenamed: 'TestJob2',
+  incorrectGitUrl: 'someurl',
+  repoErrorMessage: 'Failed to connect to repository',
 };
+
+export const ozhJenkinsLocators = {
+  jenkinsLogo: 'a.app-jenkins-logo',
+  newJobBtn: 'a[href="newJob"]',
+  newFreestyleProject: '.hudson_model_FreeStyleProject',
+  itemNameInput: 'input.jenkins-input#name',
+  okBtn: 'button#ok-button',
+  itemSidePanel: '#side-panel',
+  SCMButton: 'button[data-section-id=source-code-management]',
+  repoUrlInput: 'input[name="_.url"]',
+  repoUrlError: 'div[name="userRemoteConfigs"] .error',
+};
+
+export async function createNewItem(page: Page): Promise<void> {
+  await page.locator(ozhJenkinsLocators.newJobBtn).click();
+  await page.locator(ozhJenkinsLocators.itemNameInput).fill(ozData.jobName);
+  await page.locator(ozhJenkinsLocators.newFreestyleProject).click();
+  await page.locator(ozhJenkinsLocators.okBtn).click();
+}
