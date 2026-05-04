a6a671099d1f8c7f72db8a1f48bde7ffecee74aa
Tatiana Linder <lindertatiana@gmail.com>
2026-05-03 22:01:44 -0700

    Fix test case ID for Multibranch Pipeline test

--- tests/tl-multibranchPipeline.spec.ts
+++ tests/tl-multibranchPipeline.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect, Page } from "@/base";
 import { openNewItemPage } from "./testData/tl-data";
 
-test.describe("US_01.005 | New Item > Create Multibranch Pipeline", () => {
-  test("TC_01.005.01 | Verify Multibranch Pipeline type is displayed", async ({ page }: { page: Page }) => {
+test.describe("US_01.005 | New Item > Multibranch Pipeline ", () => {
+  test("TC_01.005.03 | Verify Multibranch Pipeline type is displayed", async ({ page }: { page: Page }) => {
     await openNewItemPage(page);
 
     await expect(page.getByText("Multibranch Pipeline")).toBeVisible();
