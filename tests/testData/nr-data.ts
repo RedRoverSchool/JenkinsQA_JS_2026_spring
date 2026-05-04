692625f4ebd92f362b1ebf36594106f9bef46071
nathalierodi <nathalierodi94@gmail.com>
2026-05-04 15:15:39 +0300

    TC_01.002.16 | Create folder from Dashboard

--- tests/testData/nr-data.ts
+++ tests/testData/nr-data.ts
@@ -2,4 +2,9 @@ export const duplicateItemNameData = {
   itemName: "TestItem",
   itemType: "Freestyle project",
   expectedWarningMessage: "A job already exists with the name",
-}
+};
+
+export const createFolderData = {
+  folderName: "TestFolder",
+  itemType: "Folder",
+};
