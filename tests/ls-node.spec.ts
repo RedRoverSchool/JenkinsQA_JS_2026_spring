dca0fc89879cbdb629cfa3df6e2ca17dc80da09e
Pitstop-git <liudmilak@yandex.ru>
2026-05-04 23:24:46 +0300

    TC_10.009.01 | Creating a new node

---
+++ tests/ls-node.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect, Page } from "../base";
+import { jenkinsData } from "./testData/ls-data";
+
+test.describe("US_10.009 | Manage Jenkins > Nodes", () => {
+	test("TC_10.009.01 | Creating a new node", async ({ page }: { page: Page }) => {
+		await page.locator("#root-action-ManageJenkinsAction").click();
+		await page.locator(".jenkins-section__item a[href='computer']").click();
+		await page.locator(".jenkins-button.jenkins-button--primary").click();
+        await page.locator("#name").fill(jenkinsData.nodeName);
+        await page.locator(".jenkins-radio__label").check();
+		await page.locator("#ok").click();
+        await page.locator("#bottom-sticker > div > button").click();
+
+		await page.locator(".app-jenkins-logo").click();
+        await page.locator("#root-action-ManageJenkinsAction").click();
+        await page.locator(".jenkins-section__item a[href='computer']").click();
+
+        const nodeLinks = await page.locator("a[href^='../computer']").allInnerTexts();
+		expect(nodeLinks).toContain(jenkinsData.nodeName);
+	});
+});
