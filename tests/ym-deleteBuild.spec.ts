e7afff688645c42207815b7fc16788dfe7958fbf
LizaMovchan <lizamovchan01@gmail.com>
2026-05-04 22:19:52 +0200

    TC_09.007.02 | Build history > Delete Build > Delete Build from Buildgs block

--- tests/ym-deleteBuild.spec.ts
+++ tests/ym-deleteBuild.spec.ts
@@ -2,7 +2,7 @@ import {test, expect, Page} from "@/base";
 import {YMdata} from "./testData/ym-data";
 
 test.describe("US_09.007 | Build history > Delete Build", ()=>{
-  test("Delete build from drop-down", async({ page }: {page :Page})=>{
+  test.beforeEach(async ({ page }:{page: Page})=>{
     await page.locator("#tasks a[href$='newJob']").click();
     await page.locator("#name").fill(YMdata.jobNameYM);
     await page.locator("#items li[class$='FreeStyleProject']").click();
@@ -10,6 +10,10 @@ test.describe("US_09.007 | Build history > Delete Build", ()=>{
 
     await page.locator("button[name = 'Submit']").click();
     await page.locator("#tasks a[href*='build']").click();
+
+  });
+
+  test("TC_09.007.01 |Delete build from drop-down", async({ page }: {page: Page})=>{
     await page.locator(".app-jenkins-logo").click();
     await page.locator("#tasks a[href$='builds']").click(); 
 
@@ -26,4 +30,15 @@ test.describe("US_09.007 | Build history > Delete Build", ()=>{
     expect(buildLoc).toHaveCount(0);
 
   });
+
+  test("TC_09.007.02 | Delete Build from Buildgs block", async({ page }: {page: Page}) => {
+    const buildLoc = page.locator("#jenkins-build-history div");
+    await page.locator(".app-builds-container__item .jenkins-card__reveal").click();
+
+    await page.locator(".jenkins-dropdown a[href$='confirmDelete']").click();
+    await page.locator("button[name='Submit']").click();
+
+    expect(buildLoc).toHaveCount(0);
+  });
+
 });
