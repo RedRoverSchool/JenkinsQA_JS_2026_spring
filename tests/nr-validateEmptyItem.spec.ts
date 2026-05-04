735058dc64d685d7922522fb4f1e232536986e2a
Natalja Rodionova <natawko94@gmail.com>
2026-05-04 13:50:44 +0300

    TC_01.001.31 | Validate Duplicate Item Name (#338)
    Co-authored-by: nathalierodi <nathalierodi94@gmail.com>

--- tests/nr-validateEmptyItem.spec.ts
+++ tests/nr-validateEmptyItem.spec.ts
@@ -1,4 +1,6 @@
 import { test, expect, Page } from "@/base";
+import { duplicateItemNameData } from "./testData/nr-data";
+
 
 test.describe("US_01.001 | New Item > Create a new item", () => {
     test("ID: TC_01.001.08 |Validate Empty Item Name", async ({ page }: { page: Page}) => {
@@ -13,4 +15,28 @@ test.describe("US_01.001 | New Item > Create a new item", () => {
        const okBtn = page.locator("button#ok-button");
        await expect(okBtn).toBeDisabled();
     });
-});
+
+    test("ID: TC_01.001.31 | Validate Duplicate Item Name", async ({ page }: { page: Page}) => {
+        // Open New Item page
+        await page.locator("#side-panel a[href$='newJob']").click();
+        // Fill in the item name
+        await page.locator("#name").fill(duplicateItemNameData.itemName);
+        // Select the Freestyle project
+        await page.getByText(duplicateItemNameData.itemType).click();
+        // Click the OK button
+        await page.locator("#ok-button").click();
+        // Go back to home page
+        await page.locator("span.jenkins-mobile-hide").click();
+        // Click New Item again
+        await page.locator("#side-panel a[href$='newJob']").click();
+        // Fill in the same item name
+        await page.locator("#name").fill(duplicateItemNameData.itemName);
+
+        const warningMsg = page.getByText(duplicateItemNameData.expectedWarningMessage);
+        await expect(warningMsg).toBeVisible();
+
+        const okBtn = page.locator("button#ok-button");
+        await expect(okBtn).toBeDisabled();
+    });
+})
+
