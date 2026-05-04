cc94c6f83cb7c481b68a24a8829936eb45286691
Pitstop-git <liudmilak@yandex.ru>
2026-05-04 23:56:36 +0300

    TC_10.009.01 | Creating a new node

--- tests/ls-node.spec.ts
+++ tests/ls-node.spec.ts
@@ -5,11 +5,13 @@ test.describe("US_10.009 | Manage Jenkins > Nodes", () => {
 	test("TC_10.009.01 | Creating a new node", async ({ page }: { page: Page }) => {
 		await page.locator("#root-action-ManageJenkinsAction").click();
 		await page.locator(".jenkins-section__item a[href='computer']").click();
-		await page.locator(".jenkins-button.jenkins-button--primary").click();
-        await page.locator("#name").fill(jenkinsData.nodeName);
+		
+		await page.getByRole("link", { name: "New Node" }).click();
+        
+		await page.locator("#name").fill(jenkinsData.nodeName);
         await page.locator(".jenkins-radio__label").check();
 		await page.locator("#ok").click();
-        await page.locator("#bottom-sticker > div > button").click();
+        await page.getByRole("button", { name: "Save" }).click();
 
         const nodeLinks = await page.locator("a[href^='../computer']").allInnerTexts();
 		expect(nodeLinks).toContain(jenkinsData.nodeName);
