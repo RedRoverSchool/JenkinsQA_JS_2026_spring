0e36eff20f6bcac9cb76f69cb5b01d6f43fd7250
Tatiana Linder <lindertatiana@gmail.com>
2026-05-04 10:08:18 -0700

    TC_05.001.06 Verify user can update folder description

--- tests/tl-folderConfiguration.spec.ts
+++ tests/tl-folderConfiguration.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect, Page } from "@/base";
-import { createFolder, folderConfigData, folderConfigLocators } from "./testData/tl-data";
+import { commonLocators, createFolder, folderConfigData, folderConfigLocators } from "./testData/tl-data";
 
 test.describe("US_05.001 | Folder Configuration > Display Name and Description", () => {
 
@@ -27,11 +27,27 @@ test.describe("US_05.001 | Folder Configuration > Display Name and Description",
 
   await page.getByText("Display Name").locator("..").locator("input").fill(folderConfigData.displayName);
 
-  await page.locator("button[name='Submit']").click();
+  await page.locator(commonLocators.submitButton).click();
 
   await expect(
   page.getByRole("heading", { name: folderConfigData.displayName })
 ).toBeVisible();
+});
+
+  test("TC_05.001.06 | Verify user can update Description", async ({ page }: { page: Page }) => {
+  await createFolder(page, folderConfigData.folderName);
+
+  await page.goto(`/job/${folderConfigData.folderName}/configure`);
+
+  const descriptionTextarea = page.locator(folderConfigLocators.descriptionTextarea);
+
+  await descriptionTextarea.clear();
+  await descriptionTextarea.fill(folderConfigData.updatedDescription);
+  await descriptionTextarea.press("Tab");
+
+  await page.locator(commonLocators.submitButton).click();
+
+  await expect(page.getByText(folderConfigData.updatedDescription, { exact: true })).toBeVisible();
 });
 
 });
