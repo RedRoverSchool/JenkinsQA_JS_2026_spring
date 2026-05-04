1585c85a72b7c56a0bc185c226e0f1025a4ac32f
Tetiana <ztv1128@gmail.com>
2026-05-04 11:47:32 -0400

    TC_12.002.02 | Persisted Sorting by Name Preference Is Applied on Dashboard Load

--- tests/testData/tz-data.ts
+++ tests/testData/tz-data.ts
@@ -1,6 +1,6 @@
 export const jenkinsData = {
     folderName: 'Folder_1',
-    generateItemName: () => `Item_${Math.random().toString(10).substring(2, 4)}`,
-    dashboardItemsCount: 6,
+    generateItemName: () => `Item_${Math.random().toString(10).substring(2, 6)}`,
+    dashboardItemsCount: 3,
     createdDashboardItems: [] as string[]
 }
