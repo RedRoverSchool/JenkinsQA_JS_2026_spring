63ebe40cfa14bd8719a2c73f5f27bd79c2abcf64
Boris S <115181391+teachandlearn999@users.noreply.github.com>
2026-05-05 01:33:18 -0700

    iframes, modals, drag and drop (#372)
    iframes, modals, drag and drop

--- tests/bs-verifyNewItem.spec.ts
+++ tests/bs-verifyNewItem.spec.ts
@@ -14,7 +14,53 @@ test.describe("US_01.001 | New Item > Create a new item", () => {
 		expect(job).toContain(jenkinsData.jobName);
 	});
 
-	test("Test_WelcomMessage", async ({ page }) => {
-		expect(page.locator("h1")).toHaveText("Welcome to Jenkins!");
+	test("drag and drop", async ({ page }: { page: Page }) => {
+		await page.goto("https://www.qa-practice.com/elements/dragndrop/boxes");
+
+		const sourceEl = page.locator("#rect-draggable");
+		const destinationEl = page.locator("#rect-droppable");
+
+		// await sourceEl.dragTo(destinationEl);
+
+		await sourceEl.hover();
+		await page.mouse.down();
+		await destinationEl.hover();
+		await page.mouse.up();
+
+		await expect(destinationEl).toContainText("Drag me");
+	});
+
+	test("iframes", async ({ page }: { page: Page }) => {
+		await page.goto("https://www.qa-practice.com/elements/iframe/iframe_page");
+
+		const iframe = page.frameLocator(".embed-responsive-item");
+
+		const buttonInsideIframe = iframe.locator(".btn-primary");
+
+		await buttonInsideIframe.click();
+
+		await expect(buttonInsideIframe).toBeVisible();
+	});
+
+	test("alert", async ({ page }: { page: Page }) => {
+		await page.goto("https://www.qa-practice.com/elements/alert/alert");
+
+		page.on("dialog", async (d) => {
+			expect(d.message()).toContain("I am an alert!");
+
+			await d.accept();
+		});
+
+		await page.locator(".a-button").click();
+	});
+
+	test("modal", async ({ page }: { page: Page }) => {
+		await page.goto("https://www.qa-practice.com/elements/popup/modal");
+
+		await page.getByRole("button", { name: "Launch Pop-Up" }).click();
+
+		const modal = page.locator("#exampleModal");
+
+		await expect(modal.getByRole("heading", { level: 5 })).toHaveText("I am a Pop-Up");
 	});
 });
