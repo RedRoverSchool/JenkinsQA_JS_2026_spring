ed75d67c1984ebd7970ccda9db234b054006147d
kdchnkv home <kadochnikovandrew@gmail.com>
2026-05-05 22:37:29 +0300

    TC_01.001.33 | New Item > Creatе a new item > Verify new item creation with unsupported symbols in name

--- tests/kd-checRedirectToCreationPage.spec.ts
+++ tests/kd-checRedirectToCreationPage.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@/base";
 import { assert } from "node:console";
-import { URLs } from "./testData/kd-data";
+import { URLs, errorText, unsupported_symbols } from "./testData/kd-data";
+import { faker } from "@faker-js/faker";
+
 
 test.describe('US_01.001 | New Item > Creatе a new item #6', () => {
     test('TC_01.001.29 | Button New Item Follow To Correct Page', async({ page }) => {
@@ -8,4 +10,15 @@ test.describe('US_01.001 | New Item > Creatе a new item #6', () => {
 
         await expect(page.url()).toContain(URLs.createNewItem);
     });
+
+    test('TC_01.001.33 | Verify new item creation with unsupported symbols in name', async({ page }) => {
+        // в рефакторе локаторы вынесу отдельно
+        await page.locator('a[href *= "/newJob"]').click();
+        
+        for (const element of unsupported_symbols){
+            // в рефакторе поменяю статичное name на фейкер
+            await page.locator('input#name').fill("name"+element);
+            await expect(page.locator('.error')).toHaveText(`‘${element}’${errorText}`);
+        }
+    })
 }); 
--- tests/testData/kd-data.ts
+++ tests/testData/kd-data.ts
@@ -1,3 +1,7 @@
 export const URLs = {
     createNewItem: "/view/all/newJob",
 }  
+
+export const unsupported_symbols =  ['?', '*', '/', '!', '%', '$', '&', ';', ':'];
+
+export const errorText = ` is an unsafe character` ;
