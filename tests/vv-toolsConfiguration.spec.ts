8effe5a1cd93368234b6eb67224fc13bf0de15cb
Valeriia Vasylyshyna <valrevina1@gmail.com>
2026-05-05 15:32:14 +0300

    fixed typo

--- tests/vv-toolsConfiguration.spec.ts
+++ tests/vv-toolsConfiguration.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, Page } from "@/base";
 
-test.describe("UUS_10.004 | Manage Jenkins > Tools", () => {
+test.describe("US_10.004 | Manage Jenkins > Tools", () => {
     test("TC_10.004.01 | Verify Tools Configuration Page opens", async ({ page }: { page: Page }) => {
         await page.locator("a[href$='manage']").click();
         await page.locator("a[href$='configureTools']").click();
