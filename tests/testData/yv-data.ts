a41cee2267c8a377905486dab749f7c158303f37
Yury Vlasko <komarcher@gmail.com>
2026-05-06 22:14:25 +0300

    TC_01.003.08-Verify creating a new item by copying configuration

--- tests/testData/yv-data.ts
+++ tests/testData/yv-data.ts
@@ -1,4 +1,4 @@
-import { th } from "@faker-js/faker";
+
 import {Page, Locator} from "@playwright/test"
 
 export const dataYV ={
@@ -6,26 +6,14 @@ export const dataYV ={
     itemName: `Item${Math.random().toString(36).substring(2,5)}`,
     nestedFolder: `Item${Math.random().toString(36).substring(2,5)}`,
     descriptionText: "Some text",
-    NewitemLink: '[href="/view/all/newJob"]',
+    NewitemLink: `[href="/view/all/newJob"]`,
     logo: '.app-jenkins-logo',
     inputName: '#name',
     freestylePr:'.hudson_model_FreeStyleProject',
     okBtn:'#ok-button',
-    descriptionField: `[name="description"].jenkins-input`,
+    descriptionField: 'textarea[name="description"]',
     saveBtn:'[name="Submit"]'
 }
-
-
-export async function descItem(page:Page){
-        await page.locator(dataYV.NewitemLink).click();
-        await page.locator(dataYV.inputName).fill(dataYV.itemName);
-        await page.locator(dataYV.freestylePr).click();
-        await page.locator(dataYV.okBtn).click();
-        await page.locator(dataYV.descriptionField).fill(dataYV.descriptionText);
-        await page.locator(dataYV.saveBtn).click();
-        await page.locator(dataYV.logo).click();
-};
-
 
 export class NewItemWhithDescription {
 
@@ -39,22 +27,22 @@ export class NewItemWhithDescription {
 
     constructor(page:Page) {
 
-        page.NewitemLink = page.locator('[href="/view/all/newJob"]');
-        page.logo = page.locator('.app-jenkins-logo');
-        page.inputName = page.getByRole('textbox', {name: 'name'});
-        page.freestylePr = page.locator('.hudson_model_FreeStyleProject');
-        page.okBtn = page.locator('#ok-button');
-        page.descriptionField = page.locator(`[name="description"].jenkins-input`);
-        page.saveBtn = page.locator('[name="Submit"]');
+        this.NewitemLink = page.locator('[href="/view/all/newJob"]');
+        this.logo = page.locator('.app-jenkins-logo');
+        this.inputName = page.locator('#name');
+        this.freestylePr = page.locator('.hudson_model_FreeStyleProject');
+        this.okBtn = page.locator('#ok-button');
+        this.descriptionField = page.locator('textarea[name="description"]');
+        this.saveBtn = page.locator('[name="Submit"]');
     };
 
     async createNewitemDesc(){
-        await page.NewitemLink.click();
-        await page.inputName.fill(dataYV.itemName);
-        await page.freestylePr.click();
-        await page.okBtn.click();
-        await page.descriptionField.fill(dataYV.descriptionText);
-        await page.saveBtn.click();
-        await page.logo.click();
+        await this.NewitemLink.click();
+        await this.inputName.fill(dataYV.itemName);
+        await this.freestylePr.click();
+        await this.okBtn.click();
+        await this.descriptionField.fill(dataYV.descriptionText);
+        await this.saveBtn.click();
+        await this.logo.click();
     };
 };
--- tests/yv-verifyCreatingByCopying.spec.ts
+++ tests/yv-verifyCreatingByCopying.spec.ts
@@ -1,8 +1,21 @@
 import {test, expect, Page} from "@/base";
-import { dataYV, descItem, NewItemWhithDescription } from "./testData/yv-data";
+import { dataYV, NewItemWhithDescription } from "./testData/yv-data";
+import { asyncWrapProviders } from "node:async_hooks";
 
 test.describe("US_01.003 | New Item > Copy from", () =>{
     test("TC_01.003.08 | Verify creating a new item by copying configuration from an existing item", async({page}:{page:Page}) =>{
-        descItem(page);
+
+        const newItem = new NewItemWhithDescription(page)
+        await newItem.createNewitemDesc();
+
+        await page.locator(dataYV.NewitemLink).click();
+        await page.locator(dataYV.inputName).fill(dataYV.nestedFolder);
+        await page.locator(dataYV.freestylePr).click();
+        await page.locator('#from').fill(dataYV.itemName);
+        await page.locator(dataYV.okBtn).click();
+        await page.locator(dataYV.saveBtn).click();
+        await page.locator(dataYV.logo).click();
+        await page.locator(`a[href$="job/${dataYV.nestedFolder}/"] span`).click({position: {x: 5, y:5 }});
+        await expect(page.locator('#description-content')).toContainText(dataYV.descriptionText);
     });
 });
