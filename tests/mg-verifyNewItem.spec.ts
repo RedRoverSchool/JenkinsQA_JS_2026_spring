754bbc5b8639282557180c991da2fb3b3341d3de
aqu1t <legomaikl@gmail.com>
2026-05-05 22:02:09 -0500

    TC_01.001.10 | Verify new item creation

---
+++ tests/mg-verifyNewItem.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@/base";
+import { Page } from "@playwright/test";
+import { jenkinsData } from "./testData/mg-data";
+
+
+test.describe("US_01.001.10 | New Item > Create New Item", () => {
+    test("TC_01.001.10 | Verify new item creation", async ({page} : { page: Page }) => {
+        await page.locator("#side-panel a[href$='newJob']").click();
+        await page.locator("#name").fill(jenkinsData.jobName);
+        await page.getByRole('radio', { name: 'Freestyle project' }).click();
+        await page.locator("#ok-button").click();        
+
+
+
+        await page.locator(".app-jenkins-logo").click();
+
+        
+    });
+});
