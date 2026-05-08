441168b73582335d60b0a4981afd557cf4fb6dcb
Tatiana Linder <89615212+lindertat@users.noreply.github.com>
2026-05-08 06:48:59 -0700

    TC_02.002.09 Verify description can be added from Status page (#428)

--- tests/testData/tl-data.ts
+++ tests/testData/tl-data.ts
@@ -66,10 +66,12 @@ export async function createFolder(page: Page, folderName: string = generateFold
   return folderName;
 }
 
-export async function createFreestyleProject(page: Page, projectName: string): Promise<void> {
+export async function createFreestyleProject(page: Page, 
+  projectName: string = generateProjectName()): Promise<string> {
   await openNewItemPage(page);
   await page.locator(newItemLocators.itemNameInput).fill(projectName);
   await page.locator(newItemLocators.freestyleProject).click();
   await page.locator(newItemLocators.okButton).click();
-  await page.locator("button[name='Submit']").click();
+  await page.locator(commonLocators.submitButton).click();
+  return projectName;
 }
