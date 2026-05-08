3b40a580376d12f3e311ef1a334d27fad4b583d6
Daria Katargina <dd.shuboderova@gmail.com>
2026-05-08 19:19:39 +0200

    TC_03.002.01 Verify editing Pipeline description

--- tests/testData/dk-data.ts
+++ tests/testData/dk-data.ts
@@ -1,3 +1,10 @@
+import { faker, Faker } from "@faker-js/faker";
+import { pipeline } from "node:stream";
+
 export const jenkinsData = {
-	folderName: "item1"
+	folderName: "item1",
+	piplineName: faker.lorem.word(),
+	piplineDesc: faker.lorem.words(),
+	pipelineDescNew: faker.lorem.words()
+
 };
