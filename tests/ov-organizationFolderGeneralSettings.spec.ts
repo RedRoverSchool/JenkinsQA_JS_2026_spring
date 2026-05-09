5b087c67892a44c25c2d11915bac85089641b48c
OlhaVatsyk <vatsykolga@gmail.com>
2026-05-09 14:39:15 -0600

    TC_07.001.03-Organization-folder-General-Settings-from-project-link-v2

--- tests/ov-organizationFolderGeneralSettings.spec.ts
+++ tests/ov-organizationFolderGeneralSettings.spec.ts
@@ -28,4 +28,16 @@ test.describe("US_07.001 | Organization folder Configuration > Change General Se
         await expect(page.locator("h2#general")).toBeVisible();
         await expect(page.locator("h2#general")).toHaveText("General");
     });
+
+    test("TC_07.001.03 | Access the General Settings from the project link", async ({page, organizationFolderName}: {page: Page; organizationFolderName: string}) => {
+        await page.locator("span.jenkins-mobile-hide").click();
+
+        const folderName = page.getByRole('link', {name: organizationFolderName});
+        await folderName.click();
+
+        await page.locator("a[href = './configure'].content-block__link").click();
+
+        await expect(page.locator("h2#general")).toBeVisible();
+        await expect(page.locator("h2#general")).toHaveText("General");
+    });
 });
