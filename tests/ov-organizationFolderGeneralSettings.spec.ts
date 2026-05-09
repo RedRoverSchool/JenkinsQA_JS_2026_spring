3b332d87cbb7097c7b230b57b8ba0266f0d3927e
OlhaVatsyk <vatsykolga@gmail.com>
2026-05-09 12:55:20 -0600

    TC_07.001.03-Organization-folder-General-Settings-from-project-link

---
+++ tests/ov-organizationFolderGeneralSettings.spec.ts
@@ -0,0 +1,46 @@
+import { test, expect, Page } from "@/tests/testFixtures/ov-organizationFolder.fixtures";
+import { string } from "@pact-foundation/pact/src/dsl/matchers";
+import { Locator } from '@playwright/test';
+
+test.describe("US_07.001 | Organization folder Configuration > Change General Settings", () => {
+    test("TC_07.001.01 | Access the General Settings from Job Dropdown on the main Jenkins page", async ({page, organizationFolderName }: {page: Page; organizationFolderName: string}) => {
+        await expect(page.getByRole("heading", { name: organizationFolderName})).toBeVisible();
+
+        await page.locator("span.jenkins-mobile-hide").click();
+        
+        const folderName = page.getByRole('link', { name: organizationFolderName });
+        await folderName.hover();
+        await folderName.getByRole("button").click();
+
+        const popup = page.locator("div[id^='tippy-'] .jenkins-dropdown");
+        await popup.getByText("Configure").click();
+
+        await expect(page.locator("h2#general")).toBeVisible();
+        await expect(page.locator("h2#general")).toHaveText("General");
+    });
+    
+    test("TC_07.001.02 | Access the General Settings by clicking the job name in the project table", async ({page, organizationFolderName }: {page: Page; organizationFolderName: string}) => {
+        await page.locator("span.jenkins-mobile-hide").click();
+        
+        const folderName = page.getByRole('link', { name: organizationFolderName });
+        await folderName.click();
+
+        await page.getByRole('link', { name: 'Configure', exact: true }).click();
+
+        await expect(page.locator("h2#general")).toBeVisible();
+        await expect(page.locator("h2#general")).toHaveText("General");
+    });
+
+    test("TC_07.001.03 | Access the General Settings from the project link", async ({page, organizationFolderName}: {page: Page; organizationFolderName: string}) => {
+        await page.locator("span.jenkins-mobile-hide").click();
+
+        const folderName = page.getByRole('link', {name: organizationFolderName});
+        await folderName.click();
+
+        await page.locator("a[href = './configure'].content-block__link").click();
+
+        await expect(page.locator("h2#general")).toBeVisible();
+        await expect(page.locator("h2#general")).toHaveText("General");
+    });
+});
+
