def70f8ccd916cdc6665ef64fdd54db4029f7194
Yury Vlasko <91124217+clin-n-izi@users.noreply.github.com>
2026-05-10 17:38:04 +0300

    TC_02.002.15 Add description to freestyle project (#493)
    * TC_02.002.13 Verify Add Description button
    
    * TC_02.002.14-Verify description field is available
    
    * TC_02.002.15-Add description to Freestyle Project

---
+++ tests/yv-Verify Add Description button is available.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect, Page} from "@/tests/testFixtures/yv-fix"
+import { dataYV } from "./testData/yv-data";
+import { createNewItem } from "./testData/ozh-data";
+
+
+
+test.describe("US_02.002 | Freestyle Project Configuration > Project Description", () =>{
+    test("TC_02.002.13 | Verify Add Description button is available in the Status section", async ({ createNewFreeProj }) =>{
+        await createNewFreeProj.locator(`a[href$="job/${dataYV.itemName}/"] span`).click({ position: { x: 5, y: 5 }});
+        await expect(createNewFreeProj.locator('[href="editDescription"]')).toBeVisible();
+    });
+
+    test("TC_02.002.14| Verify description field is available", async ({createNewFreeProj}) =>{
+
+        await createNewFreeProj.locator(`a[href$="job/${dataYV.itemName}/"] span`).click({position: {x: 5, y: 5}});
+        await createNewFreeProj.locator('[href="editDescription"]').click();
+
+        await expect(createNewFreeProj.locator('[name="description"]')).toBeVisible();
+    });
+
+    test("TC_02.002.15 | Add description to Freestyle Project", async ({createNewFreeProj}) =>{
+
+        await createNewFreeProj.locator(`a[href$="job/${dataYV.itemName}/"] span`).click({position: {x: 5, y: 5}});
+        await createNewFreeProj.locator('[href="editDescription"]').click();
+        await createNewFreeProj.locator('[name="description"]').fill(dataYV.descriptionText);
+        await createNewFreeProj.locator(dataYV.saveBtn).click();
+
+        await expect(createNewFreeProj.locator('#description-content')).toContainText(dataYV.descriptionText);
+    })
+});
+
