8eb8241b2d9de15ce5e37f0714014c8454ef8cbb
OlhaVatsyk <140862814+OlhaVatsyk@users.noreply.github.com>
2026-05-10 16:43:35 -0600

    TC_07.001.03-Organization-folder-General-Settings-from-project-link-v2 (#484)
    * TC_07.001.03-Organization-folder-General-Settings-from-project-link-v2
    
    * Left one expect

--- tests/ov-organizationFolderGeneralSettings.spec.ts
+++ tests/ov-organizationFolderGeneralSettings.spec.ts
@@ -2,8 +2,6 @@ import { test, expect, Page } from "@/tests/testFixtures/ov-organizationFolder.f
 
 test.describe("US_07.001 | Organization folder Configuration > Change General Settings", () => {
     test("TC_07.001.01 | Access the General Settings from Job Dropdown on the main Jenkins page", async ({page, organizationFolderName }: {page: Page; organizationFolderName: string}) => {
-        await expect(page.getByRole("heading", { name: organizationFolderName})).toBeVisible();
-
         await page.locator("span.jenkins-mobile-hide").click();
         
         const folderName = page.getByRole('link', { name: organizationFolderName });
@@ -13,7 +11,6 @@ test.describe("US_07.001 | Organization folder Configuration > Change General Se
         const popup = page.locator("div[id^='tippy-'] .jenkins-dropdown");
         await popup.getByText("Configure").click();
 
-        await expect(page.locator("h2#general")).toBeVisible();
         await expect(page.locator("h2#general")).toHaveText("General");
     });
     
@@ -25,7 +22,17 @@ test.describe("US_07.001 | Organization folder Configuration > Change General Se
 
         await page.getByRole('link', { name: 'Configure', exact: true }).click();
 
-        await expect(page.locator("h2#general")).toBeVisible();
+        await expect(page.locator("h2#general")).toHaveText("General");
+    });
+
+    test("TC_07.001.03 | Access the General Settings from the project link", async ({page, organizationFolderName}: {page: Page; organizationFolderName: string}) => {
+        await page.locator("span.jenkins-mobile-hide").click();
+
+        const folderName = page.getByRole('link', {name: organizationFolderName});
+        await folderName.click();
+
+        await page.locator("a[href = './configure'].content-block__link").click();
+
         await expect(page.locator("h2#general")).toHaveText("General");
     });
 });
