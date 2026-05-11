605e476074bb4cd0e560623bb01a9d29cca42cd9
Yury Vlasko <komarcher@gmail.com>
2026-05-11 22:51:57 +0300

    TC_01.04.17-Add uniqeItemName inside the loop test

--- tests/yv-EachAvailableItemTypeCreated.spec.ts
+++ tests/yv-EachAvailableItemTypeCreated.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect, Page} from "@/base";
 import { dataYV } from "./testData/yv-data";
+import { faker } from "@faker-js/faker";
 
 test.describe("US_01.004 | New Item > Select an Item type", () =>{
 
@@ -7,14 +8,16 @@ test.describe("US_01.004 | New Item > Select an Item type", () =>{
 
         test(`TC_01.004.17 | Verify that a ${itemType.name} item can be created successfully`, async({page}) =>{
 
+            const uniqeItemName = faker.string.alphanumeric(10);
+
             await page.locator(dataYV.NewitemLink).click();
-            await page.locator(dataYV.inputName).fill(dataYV.itemName);
+            await page.locator(dataYV.inputName).fill(uniqeItemName);
             await page.locator(itemType.locator).click();
             await page.locator(dataYV.okBtn).click();
             await page.locator(dataYV.logo).click();
 
             const job = await page.locator('#projectstatus .jenkins-table__link').getAttribute('href');
-            expect(job).toContain(dataYV.itemName);
+            expect(job).toContain(uniqeItemName);
         })
     }
 });
