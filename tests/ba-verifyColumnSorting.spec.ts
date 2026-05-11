6c24bbf17021f7fc2e1228167da6c201ef8c4abf
Boris Antonov <mypka69@gmail.com>
2026-05-11 16:26:19 +0200

    TC_09.002.05 | Build history > Sorting

--- tests/ba-verifyColumnSorting.spec.ts
+++ tests/ba-verifyColumnSorting.spec.ts
@@ -8,32 +8,77 @@ test.describe("US_09.002 | Build history > Sorting", () => {
 
             await page.locator(".task-link-wrapper").getByText("New Item").click();
             await page.locator('input[name="name"]').fill(jenkinsData.jobName +i);
-            await page.locator(".j-item-options").click();
+            await page.locator(".category").getByText("Freestyle project").click();
             await page.locator('button[type="submit"]').click();    
             await page.locator('button[formnovalidate="formNoValidate"]').click();
             await page.locator('.app-jenkins-logo').click();
 
         };
 
-            for (let i = 0; i < 5; i++) {
-                await page.getByRole('link', { name: /Schedule a Build/ }).nth(i).click();
-            };
+            // Build Excecution Script 
+            
+            // for (let i = 0; i < 5; i++) {
+            //     await page.getByRole('link', { name: /Schedule a Build/ }).nth(i).click();
+            // };
+
+        
     });
     
-    test("TC_09.002.05 | Build history > Sorting > Verify Name sorting", async ({ page } : { page: Page }) => {
+    test("TC_09.002.05 | Build history > Sorting > Verify 'Name' colomn sorting", async ({ page } : { page: Page }) => {
     
-        await expect(page.locator('.job-status-nobuilt')).toHaveCount(5);
-
-        const jobValues = (await page.locator(".jenkins-table__link").allInnerTexts()).map(text => text.trim());
-
-        const jobNameExample = jobValues;
+        const beforeSort = (await page.locator(".jenkins-table__link").allInnerTexts()).map(text => text.trim());
         
-        const jobNameDesc = jobNameExample.reverse();
+        const expectedDesc = [...beforeSort].sort().reverse();
         
         await page.locator(".sortheader").getByText("Name").click();
+        
+        const afterSort = (await page.locator(".jenkins-table__link").allInnerTexts()).map(text => text.trim());
 
-        await expect(jobValues).toEqual(jobNameDesc);
+        await expect(afterSort).toEqual(expectedDesc);
 
     });
-    
+
+    test("TC_09.002.06 | Build history > Sorting > Verify 'Last Success' colomn sorting", async ({ page } : {page: Page }) => {
+
+        const beforeSort = (await page.locator(".job-status-blue").nth(3).allInnerTexts()).map(text => text.trim());
+
+        const expectedDesc = [...beforeSort].sort().reverse();
+        
+        await page.locator(".sortheader").getByText("Last Success").click();
+
+        const afterSort = (await page.locator(".status-blue").nth(3).allInnerTexts()).map(text => text.trim());
+
+        await expect(afterSort).toEqual(expectedDesc);
+
+    });
+
+    test("TC_09.002.07 | Build history > Sorting > Verify 'Last Failure' colomn sorting", async ({ page } : {page: Page }) => {
+
+        const beforeSort = (await page.locator(".job-status-blue").nth(4).allInnerTexts()).map(text => text.trim());
+
+        const expectedDesc = [...beforeSort].sort().reverse();
+        
+        await page.locator(".sortheader").getByText("Last Failure").click();
+
+        const afterSort = (await page.locator(".status-blue").nth(4).allInnerTexts()).map(text => text.trim());
+
+        await expect(afterSort).toEqual(expectedDesc);
+
+    });
+
+    test("TC_09.002.07 | Build history > Sorting > Verify 'Last Duration' colomn sorting", async ({ page } : {page: Page }) => {
+
+        const beforeSort = (await page.locator(".job-status-blue").nth(5).allInnerTexts()).map(text => text.trim());
+
+        const expectedDesc = [...beforeSort].sort().reverse();
+        
+        await page.locator(".sortheader").getByText("Last Duration").click();
+
+        const afterSort = (await page.locator(".status-blue").nth(4).allInnerTexts()).map(text => text.trim());
+
+        await expect(afterSort).toEqual(expectedDesc);
+
+    });
+
+
 });
