83c2d44dbbb476ef3c59e8c0315227ac6b2e90fe
aqu1t <legomaikl@gmail.com>
2026-05-11 14:07:40 -0500

    fix: after review

--- tests/mg-verifyNewItem.spec.ts
+++ tests/mg-verifyNewItem.spec.ts
@@ -5,19 +5,18 @@ import { jenkinsData } from "./testData/mg-data";
 
 test.describe("US_01.001.10 | New Item > Create New Item", () => {
     test("TC_01.001.10 | Verify new item creation", async ({page} : { page: Page }) => {
+        
         await page.locator("#side-panel a[href$='newJob']").click();
         await page.locator("#name").fill(jenkinsData.jobName);
         await page.getByRole('radio', { name: 'Freestyle project' }).click();
-        await page.locator("#ok-button").click();        
+        await page.locator("#ok-button").click();    
 
+        await page.locator("button[name='Submit']").click();
 
+        await page.locator("#jenkins-head-icon").click();
 
-        await page.locator(".app-jenkins-logo").click();
-        const job = await page.locator("#projectstatus .jenkins-table__link").getAttribute("href");
-
-        expect(job).toContain(jenkinsData.jobName);
-
-
+        const jobLink = page.locator(`#job_${jenkinsData.jobName} .jenkins-table__link`);
+        await expect(jobLink).toBeVisible();
 
     });
 });
