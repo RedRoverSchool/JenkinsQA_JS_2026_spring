b86e0b8fff67e100fd690a334e9032d768a591fa
Tatiana Linder <lindertatiana@gmail.com>
2026-05-12 14:03:55 -0700

    RF_01.001.22 Convert blank item name validation test to POM

--- POM/tests/HomePage.spec.ts
+++ POM/tests/HomePage.spec.ts
@@ -13,4 +13,10 @@ test.describe("US_01.001 | New Item > Create a new item", () => {
 
 		await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName);
 	});
+
+	test("RF_01.001.22 | New Item > Create a new item > Verify blank item name validation", async ({ app }: { app: App }) => {
+		await app.homePage.clickNewItemLink();
+
+  		await expect(app.newItemPage.nameValidationMessage()).toContainText("This field cannot be empty");	
+	});
 });
