f9c9d1cbf4367313633972b3335df1fb730f0d7a
Tatiana Linder <89615212+lindertat@users.noreply.github.com>
2026-05-13 16:52:25 -0700

    fixed tabs

--- POM/tests/NewItemPage.spec.ts
+++ POM/tests/NewItemPage.spec.ts
@@ -2,14 +2,14 @@ import { test, expect, App } from '@/POM/fixtures/baseFixtures';
 
 test.describe('US_01.001 | New Item > Create a new item', () => {
 	test("RF_01.001.21 | New Item > Create a new item > Verify new item page opens from dashboard", async ({ app }: { app: App }) => {
-  	await app.homePage.clickNewItemLink();
+  		await app.homePage.clickNewItemLink();
 
-  	await expect(app.newItemPage.newItemTitle()).toContainText("New Item");
+  		await expect(app.newItemPage.newItemTitle()).toContainText("New Item");
 	});
     
 	test("RF_01.001.22 | New Item > Create a new item > Verify blank item name validation", async ({ app }: { app: App }) => {
 		await app.homePage.clickNewItemLink();
 
-  	await expect(app.newItemPage.itemNameValidationMessage()).toContainText("This field cannot be empty");	
+  		await expect(app.newItemPage.itemNameValidationMessage()).toContainText("This field cannot be empty");	
 	});
 });
