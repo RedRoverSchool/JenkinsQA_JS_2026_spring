8e86b038e17c13f9d8f4f9a16b3cd51913b84ce9
Tatiana Linder <89615212+lindertat@users.noreply.github.com>
2026-05-13 13:28:37 -0700

    fixed broken syntax after conflict

--- POM/tests/NewItemPage.spec.ts
+++ POM/tests/NewItemPage.spec.ts
@@ -5,6 +5,7 @@ test.describe('US_01.001 | New Item > Create a new item', () => {
   	await app.homePage.clickNewItemLink();
 
   	await expect(app.newItemPage.newItemTitle()).toContainText("New Item");
+	});
     
 	test("RF_01.001.22 | New Item > Create a new item > Verify blank item name validation", async ({ app }: { app: App }) => {
 		await app.homePage.clickNewItemLink();
