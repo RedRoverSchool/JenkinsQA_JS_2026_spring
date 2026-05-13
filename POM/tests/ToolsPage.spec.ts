9b95f42af9953ba7b3f1c7483892a02fa28d5eaa
Valeriia Vasylyshyna <valrevina1@gmail.com>
2026-05-13 16:06:49 +0300

    fixed typo

--- POM/tests/ToolsPage.spec.ts
+++ POM/tests/ToolsPage.spec.ts
@@ -7,7 +7,7 @@ test.describe("US_10.004 | Manage Jenkins > Tools", () => {
         await app.manageJenkinsPage.clickToolsLink();
     });
 
-    test("TC_10.004.01 | Verify Tools Configuration Page opens", async ({ app }: { app: App }) => {
+    test("RF_10.004.01 | Verify Tools Configuration Page opens", async ({ app }: { app: App }) => {
         await app.toolsPage.isToolsPageOpened();
         await expect(app.toolsPage.header.toolsBreadcrumb()).toBeVisible();
     });
