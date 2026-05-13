96ded46d7861fb7d053e5cfcaacf848fdd72ea0d
Tatiana Linder <lindertatiana@gmail.com>
2026-05-13 11:49:17 -0700

    added extra tab, fixed formating

--- POM/tests/NewItemPage.spec.ts
+++ POM/tests/NewItemPage.spec.ts
@@ -4,6 +4,6 @@ test.describe('US_01.001 | New Item > Create a new item', () => {
 	test("RF_01.001.22 | New Item > Create a new item > Verify blank item name validation", async ({ app }: { app: App }) => {
 		await app.homePage.clickNewItemLink();
 
-  	await expect(app.newItemPage.itemNameValidationMessage()).toContainText("This field cannot be empty");	
+  		await expect(app.newItemPage.itemNameValidationMessage()).toContainText("This field cannot be empty");	
 	});
 });
