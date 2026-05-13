fd92c01637cc11c2145d06cdcceef3656f849fd1
Tatiana Linder <lindertatiana@gmail.com>
2026-05-13 12:40:20 -0700

    Resolve merge conflict and move test to NewItemPage spec

---
+++ POM/tests/NewItemPage.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect, App } from '@/POM/fixtures/baseFixtures';
+
+test.describe('US_01.001 | New Item > Create a new item', () => {
+	test("RF_01.001.21 | New Item > Create a new item > Verify new item page opens from dashboard", async ({ app }: { app: App }) => {
+  	await app.homePage.clickNewItemLink();
+
+  	await expect(app.newItemPage.newItemTitle()).toContainText("New Item");
+	});
+});
