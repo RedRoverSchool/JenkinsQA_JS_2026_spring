d54c195cbb83e265a05ccb2eff221c755204410d
AlexM <oleksandr.motornyi@gmail.com>
2026-05-14 14:28:37 -0700

    RF_01.005.01-PrepWorkForTest

--- POM/pageObjects/pages/NewItemPage.ts
+++ POM/pageObjects/pages/NewItemPage.ts
@@ -4,7 +4,9 @@ export class NewItemPage extends BasePage {
 	itemNameField = () => this.page.locator("#name");
 	newItemTitle = () => this.page.getByRole("heading");
 	itemType_FreestyleProject = () => this.page.locator(".hudson_model_FreeStyleProject");
+	itemTypeMultibranchPipeline = () => this.page.locator(".org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject");
 	itemNameValidationMessage = () => this.page.locator("#itemname-required");
+	
 	// itemType_Pipeline = () =>
 	okButton = () => this.page.locator("#ok-button");
 
