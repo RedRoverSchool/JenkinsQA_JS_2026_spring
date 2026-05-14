c2fdbd7c259c6e44cdbf234a872482acb743323b
clin-n-izi <komarcher@gmail.com>
2026-05-14 17:03:39 +0300

    TC_02.002.17-Add description to Freestyle Project

--- POM/tests/StatusPage.spec.ts
+++ POM/tests/StatusPage.spec.ts
@@ -17,4 +17,22 @@ test.describe("US_02.002 | Freestyle Project Configuration > Project Description
 
         await expect(app.statusPage.previewTextArea()).toBeVisible();
     })
+
+    test("TC_02.002.17 | Add description to Freestyle Project", async ({app}: {app:App}) =>{
+        const itemName = faker.string.alphanumeric(10);
+        const descriptionText = faker.lorem.lines();
+
+        await app.homePage.clickNewItemLink();
+        await app.newItemPage.clickFreestyleProject();
+        await app.newItemPage.fillItemNameField(itemName);
+        await app.newItemPage.clickOkButton();
+        await app.homePage.header.clickHome()
+
+        await app.homePage.clickJobName(itemName);
+        await app.statusPage.clickAddDescriptionBtn();
+        await app.statusPage.fillDescription(descriptionText);
+        await app.statusPage.clickSaveButton();
+
+        await expect(app.statusPage.descriptionContent()).toContainText(descriptionText);
+    })
 })
