803b2beac578d3611b317a38da27a850d90e0a61
vida65979-arch <vida65979@gmail.com>
2026-05-15 11:21:18 +0600

    RF_05.003.01 | Verify adding Pipeline library test in POM (#553)
    * RF_05.003.01 | Verify adding Pipeline library test in POM
    
    * RF_05.003.01 | Verify adding Pipeline library test in POM
    
    * RF_05.003.01 | Verify adding Pipeline library test in POM
    
    * RF_05.003.01 | Verify adding Pipeline library test in POM
    
    * RF_05.003.01 | Verify adding Pipeline library test in POM
    
    * RF_05.003.01 | Verify adding Pipeline library test in POM
    
    ---------
    
    Co-authored-by: vida65979-arch <vida65979@gmai.com>

--- POM/pageObjects/App.ts
+++ POM/pageObjects/App.ts
@@ -1,3 +1,5 @@
+import {ConfigureFolderPage} from "@/POM/pageObjects/pages/ConfigureFolderPage";
+import {FolderPage} from "@/POM/pageObjects/pages/FolderPage";
 import { Page } from '@playwright/test';
 import { HomePage } from './pages/HomePage';
 import { NewItemPage } from './pages/NewItemPage';
@@ -8,41 +10,52 @@ import { ToolsPage } from './pages/ToolsPage';
 import { PluginsPage } from './pages/PluginsPage';
 
 export class App {
-	private _homePage: HomePage | null = null;
-	private _newItemPage: NewItemPage | null = null;
-	private _configureFreestylePage: ConfigureFreestylePage | null = null;
-	private _freestyleProjectPage: FreestyleProjectPage | null = null;
-	private _manageJenkinsPage: ManageJenkinsPage | null = null;
-	private _toolsPage: ToolsPage | null = null;
+  private _homePage: HomePage | null = null;
+  private _newItemPage: NewItemPage | null = null;
+  private _configureFreestylePage: ConfigureFreestylePage | null = null;
+  private _freestyleProjectPage: FreestyleProjectPage | null = null;
+  private _manageJenkisPage: ManageJenkinsPage | null = null;
+  private _toolsPage: ToolsPage | null = null;
+  private _configureFolderPage: ConfigureFolderPage | null = null;
+  private _folderPage: FolderPage | null = null;
 	private _pluginsPage: PluginsPage | null = null;
 
-	constructor(private readonly page: Page) { }
+    constructor(private readonly page: Page) {
+    }
 
-	get homePage() {
-		return (this._homePage ??= new HomePage(this.page));
-	}
+    get homePage() {
+        return (this._homePage ??= new HomePage(this.page));
+    }
 
-	get newItemPage() {
-		return (this._newItemPage ??= new NewItemPage(this.page));
-	}
+    get newItemPage() {
+        return (this._newItemPage ??= new NewItemPage(this.page));
+    }
 
-	get configureFreestylePage() {
-		return (this._configureFreestylePage ??= new ConfigureFreestylePage(this.page));
-	}
+    get configureFreestylePage() {
+        return (this._configureFreestylePage ??= new ConfigureFreestylePage(this.page));
+    }
 
-	get freeStyleProjectPage() {
-		return (this._freestyleProjectPage ??= new FreestyleProjectPage(this.page));
-	}
+    get configureFolderPage() {
+        return (this._configureFolderPage ??= new ConfigureFolderPage(this.page));
+    }
 
-	get manageJenkinsPage() {
-		return (this._manageJenkinsPage ??= new ManageJenkinsPage(this.page));
-	}
+    get folderPage() {
+        return (this._folderPage ??= new FolderPage(this.page));
+    }
 
-	get toolsPage() {
-		return (this._toolsPage ??= new ToolsPage(this.page));
-	}
+    get freeStyleProjectPage() {
+      return (this._freestyleProjectPage ??= new FreestyleProjectPage(this.page));
+    }
 
-	get pluginsPage() {
-		return (this._pluginsPage ??= new PluginsPage(this.page));
-	}
+    get manageJenkinsPage() {
+      return (this._manageJenkisPage ??= new ManageJenkinsPage(this.page));
+    }
+
+    get toolsPage() {
+      return (this._toolsPage ??= new ToolsPage(this.page));
+    }
+
+    get pluginsPage() {
+      return (this._pluginsPage ??= new PluginsPage(this.page));
+    }
 }
