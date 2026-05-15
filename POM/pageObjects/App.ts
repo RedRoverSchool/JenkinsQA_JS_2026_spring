0e2f6efa02542ac8edbabff9a1636c0eb6ed6d48
Oksana <okepych@yahoo.com>
2026-05-14 23:48:19 -0500

    RF_10.005.01 | Verify user can access Plugins section (#537)
    RF_10.005.01 | Verify user can access Plugins section

--- POM/pageObjects/App.ts
+++ POM/pageObjects/App.ts
@@ -5,38 +5,44 @@ import { ConfigureFreestylePage } from './pages/ConfigureFreestylePage';
 import { FreestyleProjectPage } from './pages/FreestyleProjectPage';
 import { ManageJenkinsPage } from './pages/ManageJenkinsPage';
 import { ToolsPage } from './pages/ToolsPage';
+import { PluginsPage } from './pages/PluginsPage';
 
 export class App {
-  private _homePage: HomePage | null = null;
-  private _newItemPage: NewItemPage | null = null;
-  private _configureFreestylePage: ConfigureFreestylePage | null = null;
-  private _freestyleProjectPage: FreestyleProjectPage | null = null;
-  private _manageJenkisPage: ManageJenkinsPage | null = null;
-  private _toolsPage: ToolsPage | null = null;
+	private _homePage: HomePage | null = null;
+	private _newItemPage: NewItemPage | null = null;
+	private _configureFreestylePage: ConfigureFreestylePage | null = null;
+	private _freestyleProjectPage: FreestyleProjectPage | null = null;
+	private _manageJenkinsPage: ManageJenkinsPage | null = null;
+	private _toolsPage: ToolsPage | null = null;
+	private _pluginsPage: PluginsPage | null = null;
 
-  constructor(private readonly page: Page) {}
+	constructor(private readonly page: Page) { }
 
-  get homePage() {
-    return (this._homePage ??= new HomePage(this.page));
-  }
+	get homePage() {
+		return (this._homePage ??= new HomePage(this.page));
+	}
 
-  get newItemPage() {
-    return (this._newItemPage ??= new NewItemPage(this.page));
-  }
+	get newItemPage() {
+		return (this._newItemPage ??= new NewItemPage(this.page));
+	}
 
-  get configureFreestylePage() {
-    return (this._configureFreestylePage ??= new ConfigureFreestylePage(this.page));
-  }
+	get configureFreestylePage() {
+		return (this._configureFreestylePage ??= new ConfigureFreestylePage(this.page));
+	}
 
-  get freeStyleProjectPage() {
-    return (this._freestyleProjectPage ??= new FreestyleProjectPage(this.page));
-  }
+	get freeStyleProjectPage() {
+		return (this._freestyleProjectPage ??= new FreestyleProjectPage(this.page));
+	}
 
-  get manageJenkinsPage() {
-    return (this._manageJenkisPage ??= new ManageJenkinsPage(this.page));
-  }
+	get manageJenkinsPage() {
+		return (this._manageJenkinsPage ??= new ManageJenkinsPage(this.page));
+	}
 
-  get toolsPage() {
-    return (this._toolsPage ??= new ToolsPage(this.page));
-  }
+	get toolsPage() {
+		return (this._toolsPage ??= new ToolsPage(this.page));
+	}
+
+	get pluginsPage() {
+		return (this._pluginsPage ??= new PluginsPage(this.page));
+	}
 }
