8406b26583fa89950ea1af8cbbdd77218ce5071d
BorisS <teachandlearn999@gmail.com>
2026-05-14 22:03:30 -0700

    testing ci

--- POM/tests/FolderPage.spec.ts
+++ POM/tests/FolderPage.spec.ts
@@ -2,19 +2,15 @@ import { test, expect, App } from "@/POM/fixtures/baseFixtures";
 import { newItemPageData } from "@/POM/testData/newItemPageData";
 
 test.describe("US_05.001 | Folder Configuration > Display Name and Description", () => {
-  test("RF_05.001.03 | Folder Configuration > Display Name and Description > Verify Configure page opens", async ({
-    app,
-  }: {
-    app: App;
-  }) => {
-    await app.homePage.clickNewItemLink();
-    await app.newItemPage.fillItemNameField(newItemPageData.folderName);
-    await app.newItemPage.clickFolder();
-    await app.newItemPage.clickOkButton();
+	test("RF_05.001.03 | Verify Configure page opens", async ({ app }: { app: App }) => {
+		await app.homePage.clickNewItemLink();
+		await app.newItemPage.fillItemNameField(newItemPageData.folderName);
+		await app.newItemPage.clickFolder();
+		await app.newItemPage.clickOkButton();
 
-    await app.configureFolderPage.clickSaveButton();
-    await app.folderPage.clickConfigureMenuItem();
+		await app.configureFolderPage.clickSaveButton();
+		await app.folderPage.clickConfigureMenuItem();
 
-    await expect(app.configureFolderPage.generalSection()).toBeVisible();
-  });
+		await expect(app.configureFolderPage.generalSection()).toBeVisible();
+	});
 });
