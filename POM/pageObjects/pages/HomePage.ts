a5883243e610e6f7808b8b4519f04ca3281f1437
BorisS <teachandlearn999@gmail.com>
2026-05-14 23:39:09 -0700

    test ci

--- POM/pageObjects/pages/HomePage.ts
+++ POM/pageObjects/pages/HomePage.ts
@@ -1,44 +1,46 @@
-import { BasePage } from './@components';
+import { BasePage } from "./@components";
 
 export class HomePage extends BasePage {
-  // locators
-  newItemLink = () => this.page.locator("#side-panel a[href$='newJob']");
-  itemName = () => this.page.locator('#projectstatus .jenkins-table__link');
-  itemDropDownConfigureButton = (jobName: string) => this.page.locator(`a[href*='/job/${jobName}/configure']`);
-  projectTableRow = (itemName: string) => this.page.locator(`tr#job_${itemName}`);
-  itemDropDown = () => this.page.locator(`div.jenkins-dropdown`);
-  deleteProjectInDropdownBtn = (itemName: string) =>
-    this.itemDropDown().locator(`button[href*="job/${itemName}/doDelete"].jenkins-dropdown__item`);
-  itemMenuChevron = () => this.page.locator('button.jenkins-menu-dropdown-chevron');
-  confirmDeleteBtn = () => this.page.locator('button[data-id=ok]');
-  // actions
-  async clickNewItemLink() {
-    await this.newItemLink().click();
-  }
+	// locators
+	newItemLink = () => this.page.locator("#side-panel a[href$='newJob']");
+	itemName = () => this.page.locator("#projectstatus .jenkins-table__link");
+	itemDropDownConfigureButton = (jobName: string) =>
+		this.page.locator(`a[href*='/job/${jobName}/configure']`);
+	projectTableRow = (itemName: string) => this.page.locator(`tr#job_${itemName}`);
+	itemDropDown = () => this.page.locator(`div.jenkins-dropdown`);
+	deleteProjectInDropdownBtn = (itemName: string) =>
+		this.itemDropDown().locator(`button[href*="job/${itemName}/doDelete"].jenkins-dropdown__item`);
+	itemMenuChevron = () => this.page.locator("button.jenkins-menu-dropdown-chevron");
+	confirmDeleteBtn = () => this.page.locator("button[data-id=ok]");
+	// actions
+	async clickNewItemLink() {
+		await this.newItemLink().click();
+	}
 
-  async clickItemNameLink() {
-    await this.itemName().focus();
-    await this.itemName().press('Enter');
-  }
+	async clickItemNameLink() {
+		await this.itemName().focus();
+		await this.itemName().press("Enter");
+	}
 
-  async hoverItemName() {
-    await this.itemName().hover();
-    return this;
-  }
-  async clickDeleteProjectInDropdown(itemName: string) {
-    await this.deleteProjectInDropdownBtn(itemName).click();
-    return this;
-  }
+	async hoverItemName() {
+		await this.itemName().hover();
+		return this;
+	}
+	async clickDeleteProjectInDropdown(itemName: string) {
+		await this.deleteProjectInDropdownBtn(itemName).click();
+		return this;
+	}
 
-  async openItemDropdownMenu() {
-    await this.itemMenuChevron().click();
-    return this;
-  }
+	async openItemDropdownMenu() {
+		await this.itemMenuChevron().click();
+		return this;
+	}
 
-  async clickItemDropDownConfigureButton(jobName: string) {
-    await this.itemDropDownConfigureButton(jobName).click();}
+	async clickItemDropDownConfigureButton(jobName: string) {
+		await this.itemDropDownConfigureButton(jobName).click();
+	}
 
-  async clickConfirmDeleteBtn() {
-    await this.confirmDeleteBtn().click();
-  }
+	async clickConfirmDeleteBtn() {
+		await this.confirmDeleteBtn().click();
+	}
 }
