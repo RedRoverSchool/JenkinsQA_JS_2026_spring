24eedb3ebe23bf1bf761e9862edf7790cc83e65b
AlexM <oleksandr.motornyi@gmail.com>
2026-05-14 17:27:37 -0700

    Changed the locator

--- POM/pageObjects/pages/NewItemPage.ts
+++ POM/pageObjects/pages/NewItemPage.ts
@@ -4,7 +4,7 @@ export class NewItemPage extends BasePage {
 	itemNameField = () => this.page.locator("#name");
 	newItemTitle = () => this.page.getByRole("heading");
 	itemType_FreestyleProject = () => this.page.locator(".hudson_model_FreeStyleProject");
-	itemTypeMultibranchPipeline = () => this.page.locator(".org_jenkinsci_plugins_workflow_multibranch_WorkflowMultiBranchProject");
+	itemTypeMultibranchPipeline = () => this.page.getByLabel("Multibranch Pipeline");
 	itemNameValidationMessage = () => this.page.locator("#itemname-required");
 	
 	// itemType_Pipeline = () =>
