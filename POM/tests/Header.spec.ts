58aff390252cacf4cbc71a3d686ad6999a8c644b
Evgeny Chernyavskiy <evgg90@gmail.com>
2026-05-15 17:20:53 +0700

    RF_13.001.02 | Verify redirection to Dashboard and content visibility

--- POM/tests/Header.spec.ts
+++ POM/tests/Header.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, App } from '@/POM/fixtures/baseFixtures';
+import { newItemPageData } from "../testData/newItemPageData";
 
 test.describe('US_13.001 | Header > Navigate to Dashboard', () => {
     test('RF_13.001.01 | Verify header is visible from tools page', async ({ app }: { app: App }) => {
@@ -6,6 +7,17 @@ test.describe('US_13.001 | Header > Navigate to Dashboard', () => {
         await app.manageJenkinsPage.clickTools();
 
         await expect (app.toolsPage.header.logoLink()).toBeVisible();
+    });
+
+     test('RF_13.001.02 | Verify redirection to Dashboard and content visibility', async ({ app }: { app: App }) => {
+        await app.homePage.clickNewItemLink();
+        await app.newItemPage.fillItemNameField(newItemPageData.itemName);
+        await app.newItemPage.clickFreestyleProject();
+        await app.newItemPage.clickOkButton();
+
+        await app.configureFreestylePage.header.clickHome();
+
+        await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName);
     });
 
 });
