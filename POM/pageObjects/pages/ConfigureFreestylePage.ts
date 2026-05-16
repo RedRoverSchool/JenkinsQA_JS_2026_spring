9d6366f000c9b9f19ec446d041b7661195a921b2
himym1989 <himym1989@gmail.com>
2026-05-16 14:58:24 +0300

    TC_02.004.02 | Verify Authentication Token field is displayed when "Trigger builds remotely" is selected

--- POM/pageObjects/pages/ConfigureFreestylePage.ts
+++ POM/pageObjects/pages/ConfigureFreestylePage.ts
@@ -5,6 +5,9 @@ export class ConfigureFreestylePage extends BasePage {
   saveChangesBtn = () => this.page.locator(".jenkins-submit-button");
   triggersSectionBtn = () => this.page.locator('button[data-section-id="triggers"]');
   triggersSectionTitle = () => this.page.locator("div.jenkins-section__title#triggers");
+  authTokenField = () => this.page.locator('input[name="authToken"]');
+  triggerBuildsRemotelyCheckbox = () =>
+    this.page.locator("label.attach-previous").filter({ hasText: "Trigger builds remotely" });
 
   async disableProject() {
     await this.enableProjectSwitcher().uncheck();
@@ -16,6 +19,11 @@ export class ConfigureFreestylePage extends BasePage {
 
   async goToTriggersSection() {
     await this.triggersSectionBtn().click();
+    return this;
+  }
+
+  async checkTriggerBuildsRemotely() {
+    await this.triggerBuildsRemotelyCheckbox().click();
     return this;
   }
 }
--- POM/tests/ConfigureFreestyleProject.spec.ts
+++ POM/tests/ConfigureFreestyleProject.spec.ts
@@ -2,18 +2,30 @@ import { test, expect, App } from "@/POM/fixtures/baseFixtures";
 import { newItemPageData } from "../testData/newItemPageData";
 
 test.describe("US_02.004 | Freestyle Project Configuration > Build Triggers", () => {
-  test("TC_02.004.01 | Verify Build Triggers section is available during project setup", async ({
-    app,
-  }: {
-    app: App;
-  }) => {
+  test.beforeEach(async ({ app }: { app: App }) => {
     await app.homePage.clickNewItemLink();
     await app.newItemPage.fillItemNameField(newItemPageData.itemName);
     await app.newItemPage.clickFreestyleProject();
     await app.newItemPage.clickOkButton();
     await app.configureFreestylePage.goToTriggersSection();
+  });
 
+  test("TC_02.004.01 | Verify Build Triggers section is available during project setup", async ({
+    app,
+  }: {
+    app: App;
+  }) => {
     await expect(app.configureFreestylePage.triggersSectionTitle()).toBeInViewport();
     await expect(app.configureFreestylePage.triggersSectionTitle()).toContainText("Triggers");
+  });
+
+  test(`TC_02.004.02 | Verify Authentication Token field is displayed when "Trigger builds remotely" is selected`, async ({
+    app,
+  }: {
+    app: App;
+  }) => {
+    await app.configureFreestylePage.checkTriggerBuildsRemotely();
+    await expect(app.configureFreestylePage.authTokenField()).toBeVisible();
+    await expect(app.configureFreestylePage.authTokenField()).toBeEnabled();
   });
 });
