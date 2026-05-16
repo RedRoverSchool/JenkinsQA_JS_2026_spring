6fe7e401a6dc245d5488f40652935cf52d0fda07
himym1989 <himym1989@gmail.com>
2026-05-16 23:28:32 +0300

    TC_11.003.01 | Verify Manage Jenkins page contains all grouped sections

--- POM/pageObjects/pages/@components/Header.ts
+++ POM/pageObjects/pages/@components/Header.ts
@@ -1,18 +1,19 @@
 import { Page } from "@playwright/test";
 
 export class Header {
-	constructor(private readonly page: Page) {}
+  constructor(private readonly page: Page) {}
 
-	// locators
-	logoLink = () => this.page.locator(".app-jenkins-logo");
-	gearLink = () => this.page.locator("#root-action-ManageJenkinsAction");
+  // locators
+  logoLink = () => this.page.locator(".app-jenkins-logo");
+  gearLink = () => this.page.locator("#root-action-ManageJenkinsAction");
 
-	// actions
-	async clickHome() {
-		await this.logoLink().click();
-	}
+  // actions
+  async clickHome() {
+    await this.logoLink().click();
+  }
 
-	async clickManageJenkins() {
-		await this.gearLink().click();
-	}
+  async clickManageJenkins() {
+    await this.gearLink().click();
+    await this.page.waitForLoadState("domcontentloaded");
+  }
 }
