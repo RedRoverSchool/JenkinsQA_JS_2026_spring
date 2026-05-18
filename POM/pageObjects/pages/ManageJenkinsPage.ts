0386a27e600fb1687bbe60391e26547f4ac6c05a
Olena Zhukova <72292429+himym1989@users.noreply.github.com>
2026-05-18 19:06:54 +0300

    TC_11.003.02 | Verify search input filters settings and options (#619)

--- POM/pageObjects/pages/ManageJenkinsPage.ts
+++ POM/pageObjects/pages/ManageJenkinsPage.ts
@@ -4,6 +4,9 @@ export class ManageJenkinsPage extends BasePage {
   toolsLink = () => this.page.locator("[href='configureTools']");
   pluginsLink = () => this.page.getByRole("link", { name: "Plugins Add, remove, disable" });
   jenkinsSectionTitle = () => this.page.locator("h2.jenkins-section__title");
+  settingsSearchBar = () => this.page.locator("#settings-search-bar");
+  searchBarDropdownItem = () =>
+    this.page.locator(".jenkins-search__results-container .jenkins-dropdown__item");
 
   async clickTools() {
     await this.toolsLink().click();
