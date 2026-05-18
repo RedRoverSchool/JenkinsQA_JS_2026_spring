2f251829c8cb2f61db9754da4794eb66ef671b03
LizaMovchan <74600570+LizaMovchan@users.noreply.github.com>
2026-05-18 19:03:41 +0200

    RF_09.001.01-Item displays on Build History page after building (#605)

--- POM/tests/BuildHistoryPage.spec.ts
+++ POM/tests/BuildHistoryPage.spec.ts
@@ -20,4 +20,20 @@ test.describe("US_09.001 | Build history > Core Build History Display", () => {
       app.buildHistoryPage.successfulBuildStatusIcon(projectName),
     ).toBeVisible();
   });
+
+  test("RF_09.001.01 | Item displays on Build History page after building", async({ app })=>{
+
+    await app.homePage.clickNewItemLink();
+    await app.newItemPage.fillItemNameField(newItemPageData.itemName)
+    await app.newItemPage.clickFreestyleProject();
+    await app.newItemPage.clickOkButton();
+
+    await app.configureFreestylePage.saveChanges();
+    await app.freeStyleProjectPage.clickBuildNowLink();
+    await app.freeStyleProjectPage.header.clickHome();
+    await app.homePage.clickBuildHistoryLink();
+    
+    await expect(app.buildHistoryPage.newItemName()).toContainText(newItemPageData.itemName);
+  });
+
 });
