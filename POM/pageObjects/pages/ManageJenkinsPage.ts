7554b3f1182a68b7ed42d0c35b68ecdd1cb13f0a
himym1989 <himym1989@gmail.com>
2026-05-18 16:43:29 +0300

    TC_11.003.02 | Verify search input filters settings and options

--- POM/pageObjects/pages/ManageJenkinsPage.ts
+++ POM/pageObjects/pages/ManageJenkinsPage.ts
@@ -4,6 +4,8 @@ export class ManageJenkinsPage extends BasePage {
   toolsLink = () => this.page.locator("[href='configureTools']");
   pluginsLink = () => this.page.getByRole("link", { name: "Plugins Add, remove, disable" });
   jenkinsSectionTitle = () => this.page.locator("h2.jenkins-section__title");
+  settingsSearchBar = () => this.page.locator("#settings-search-bar");
+  searchBarDropdownItem = () => this.page.locator(".jenkins-dropdown__item");
 
   async clickTools() {
     await this.toolsLink().click();
