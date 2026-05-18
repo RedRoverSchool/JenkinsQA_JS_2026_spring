236852ac6eb3f43cc0d648c295b28b6a060a6e09
aqu1t <legomaikl@gmail.com>
2026-05-18 18:38:07 -0500

    RF_01.001.10 | New item > Create new item > Verify error message for existing item (#565)
    * RF_01.001.10 Verify error message for existing item
    
    * Update NewItemPage.ts
    
    * Update ConfigureFreestylePage.ts
    
    * Fix: fix after review

--- POM/pageObjects/pages/NewItemPage.ts
+++ POM/pageObjects/pages/NewItemPage.ts
@@ -1,21 +1,31 @@
 import { BasePage } from './@components';
 
 export class NewItemPage extends BasePage {
-  itemNameField = () => this.page.locator('#name');
-  newItemTitle = () => this.page.getByRole('heading');
-  itemType_FreestyleProject = () => this.page.locator('.hudson_model_FreeStyleProject');
-  itemType_Folder = () => this.page.locator('.com_cloudbees_hudson_plugins_folder_Folder');
-  itemNameValidationMessage = () => this.page.locator('#itemname-required');
+  itemNameField = () => this.page.locator("#name");
+  newItemTitle = () => this.page.getByRole("heading");
+
+  itemType_FreestyleProject = () =>
+    this.page.locator(".hudson_model_FreeStyleProject");
+
+  itemType_Folder = () =>
+    this.page.locator(".com_cloudbees_hudson_plugins_folder_Folder");
+
+  itemNameValidationMessage = () =>
+    this.page.locator("#itemname-required");
+
+  errorMessage = () =>
+    this.page.locator("#itemname-invalid");
+
   itemType_Pipeline = () =>
-    this.page.locator(
-      "[class='org_jenkinsci_plugins_workflow_job_WorkflowJob']",
-    );
+    this.page.locator("[class='org_jenkinsci_plugins_workflow_job_WorkflowJob']");
+
   duplicateItemNameWarning = () =>
     this.page.getByText("A job already exists with the name");
+
   itemType_OrganizationFolder = () =>
     this.page.getByRole("radio", { name: "Organization Folder Creates a" });
-  // itemType_Pipeline = () =>
-  okButton = () => this.page.locator('#ok-button');
+
+  okButton = () => this.page.locator("#ok-button");
   itemTypesOptions = () => this.page.locator('.j-item-options .label');
 
   async fillItemNameField(name: string) {
@@ -29,6 +39,20 @@ export class NewItemPage extends BasePage {
   }
 
   async clickOkButton() {
+    await this.okButton().click();
+  }
+
+  async getErrorText() {
+    return await this.errorMessage().innerText();
+  }
+
+  async clickFolder() {
+    await this.itemType_Folder().click();
+    return this;
+  }
+
+  async clickFolderAndOkButton() {
+    await this.itemType_Folder().click();
     await this.okButton().click();
   }
 
@@ -46,16 +70,6 @@ export class NewItemPage extends BasePage {
     await this.fillItemNameField(name);
     await this.clickFreestyleProject();
     await this.clickOkButton();
-  }
-
-  async clickFolder() {
-    await this.itemType_Folder().click();
-    return this;
-  }
-
-  async clickFolderAndOkButton() {
-    await this.itemType_Folder().click();
-    await this.okButton().click();
   }
 
   async clickOrganizationFolder() {
