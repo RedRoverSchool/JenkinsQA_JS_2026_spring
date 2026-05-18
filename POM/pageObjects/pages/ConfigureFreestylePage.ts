87a5775a2fcacec346fcfc7d0ca2777149771531
Olena Zhukova <72292429+himym1989@users.noreply.github.com>
2026-05-18 12:38:57 +0300

    TC_02.004.02 | Verify Authentication Token field is displayed when "Trigger builds remotely" is selected (#595)

--- POM/pageObjects/pages/ConfigureFreestylePage.ts
+++ POM/pageObjects/pages/ConfigureFreestylePage.ts
@@ -5,6 +5,9 @@ export class ConfigureFreestylePage extends BasePage {
   saveChangesBtn = () => this.page.locator(".jenkins-submit-button");
   triggersSectionBtn = () => this.page.locator('button[data-section-id="triggers"]');
   triggersSectionTitle = () => this.page.locator("div.jenkins-section__title#triggers");
+  authTokenField = () => this.page.locator('input[name="authToken"]');
+  triggerBuildsRemotelyCheckbox = () =>
+    this.page.locator("label.attach-previous").filter({ hasText: "Trigger builds remotely" });
 
   async disableProject() {
     await this.enableProjectSwitcher().uncheck();
@@ -17,6 +20,11 @@ export class ConfigureFreestylePage extends BasePage {
 
   async goToTriggersSection() {
     await this.triggersSectionBtn().click();
+    return this;
+  }
+
+  async enableTriggerBuildsRemotely() {
+    await this.triggerBuildsRemotelyCheckbox().click();
     return this;
   }
 }
