f695e32ff50a94396c5bb45962be8bac750de0ac
Boris Antonov <mypka69@gmail.com>
2026-05-18 17:16:24 +0200

    RF_15.001.05-Verify-footer-dropdown-get-involved-url

--- POM/pageObjects/pages/@components/Footer.ts
+++ POM/pageObjects/pages/@components/Footer.ts
@@ -6,7 +6,7 @@ export class Footer {
 	jenkinsVersionButton = () => this.page.locator("button.jenkins_ver");
 	jenkinsVersionDropdown = () => this.page.locator(".jenkins-dropdown");
 	jenkinsVersionDropdownItems = () => this.page.locator(".jenkins-dropdown__item");
-	jenkinsVersionDropdownAbout = () => this.page.getByText("Get involved");
+	jenkinsVersionDropdownAbout = () => this.page.getByText("About Jenkins");
 	jenkinsVersionDropdownGetInvolved = () => this.page.locator(`[href="https://www.jenkins.io/participate/"]`);
 	jenkinsVersionDropdownWebsite = () => this.page.locator(`[href="https://www.jenkins.io/"]`);
 
