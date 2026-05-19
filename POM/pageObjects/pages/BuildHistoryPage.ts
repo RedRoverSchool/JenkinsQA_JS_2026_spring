fccd743868555dfd3828ddf74ebc878ba7d1e83a
Tatiana Linder <lindertatiana@gmail.com>
2026-05-19 10:00:31 -0700

    Apply 4-space indentation formatting

--- POM/pageObjects/pages/BuildHistoryPage.ts
+++ POM/pageObjects/pages/BuildHistoryPage.ts
@@ -1,27 +1,27 @@
 import { BasePage } from "./@components";
 
 export class BuildHistoryPage extends BasePage {
-  successfulBuildEntry = (projectName: string) =>
-    this.page.locator("tr", { hasText: projectName });
-  newItemName = () =>
-    this.page.locator("#projectStatus .jenkins-table__link span");
-  successfulBuildStatusIcon = (projectName: string) =>
-    this.successfulBuildEntry(projectName).locator("svg#blue");
-  buildValues = () => this.page.getByText(/^#\d+$/);
-  sortableBuildHeader = () =>
-    this.page.locator("th[initialsortdir='up'] a.sortheader");
-  firstBuildNumberLink = () => this.buildValues().first();
+    successfulBuildEntry = (projectName: string) =>
+        this.page.locator("tr", { hasText: projectName });
+    newItemName = () =>
+        this.page.locator("#projectStatus .jenkins-table__link span");
+    successfulBuildStatusIcon = (projectName: string) =>
+        this.successfulBuildEntry(projectName).locator("svg#blue");
+    buildValues = () => this.page.getByText(/^#\d+$/);
+    sortableBuildHeader = () =>
+        this.page.locator("th[initialsortdir='up'] a.sortheader");
+    firstBuildNumberLink = () => this.buildValues().first();
 
-  async clickSortableBuildHeader() {
-    await this.sortableBuildHeader().click();
-    return this;
-  }
+    async clickSortableBuildHeader() {
+        await this.sortableBuildHeader().click();
+        return this;
+    }
 
-  async getBuildValues() {
-    return this.buildValues().allTextContents();
-  }
+    async getBuildValues() {
+        return this.buildValues().allTextContents();
+    }
 
-  async getFirstBuildNumber() {
-    return this.firstBuildNumberLink().textContent();
-  }
+    async getFirstBuildNumber() {
+        return this.firstBuildNumberLink().textContent();
+    }
 }
