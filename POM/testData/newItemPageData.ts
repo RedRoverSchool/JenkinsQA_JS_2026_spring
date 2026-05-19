8b00f33be6d04aac012daf66c56322b28f740f39
zina6916 <191519830+zina6916@users.noreply.github.com>
2026-05-19 11:47:37 -0400

    TC_01.004.16 Verify validation message for invalid input POM (#586)
    TC_01.004.16 Verify validation message for invalid input

--- POM/testData/newItemPageData.ts
+++ POM/testData/newItemPageData.ts
@@ -1,7 +1,8 @@
 import { faker } from "@faker-js/faker";
 
-export const newItemPageData = {
+ export const newItemPageData = {
 	itemName: faker.word.noun(),
+	invalidItemName: "Build@Test",
 	folderName: `folder-${faker.string.alphanumeric(8)}`,
 	itemTypes: {
 		pipeline: "Pipeline",
