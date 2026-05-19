65c86d735c7e3cc60c51c51e800c303054ccaa2e
Boris S <115181391+teachandlearn999@users.noreply.github.com>
2026-05-18 23:52:22 -0700

    Common describe and Accessibility (#643)

--- POM/tests/HomePage.spec.ts
+++ POM/tests/HomePage.spec.ts
@@ -1,102 +1,126 @@
-import { test, expect, App } from '@/POM/fixtures/baseFixtures';
-import { newItemPageData } from '../testData/newItemPageData';
-import { footer } from '../testData/jenkinsData';
-import { newItemData } from '@/tests/testData/tl-data';
+import { test, expect, App } from "@/POM/fixtures/newItemPageFixtures";
+import { newItemPageData } from "../testData/newItemPageData";
+import { footer } from "../testData/jenkinsData";
 
-test.describe('US_01.001 | New Item > Create a new item', () => {
-  test('RF_01.001.01 | Verify new item creation', async ({ app }: { app: App }) => {
-    await app.homePage.clickNewItemLink();
+test.describe("Home Page Tests", () => {
+	test("RF_01.001.01 | Verify new item creation", async ({ app, createJob }) => {
+		await createJob(newItemPageData.itemName, newItemPageData.itemTypes.freestyleProject, true);
 
-    await app.newItemPage.fillItemNameField(newItemPageData.itemName);
-    await app.newItemPage.clickFreestyleProject();
-    await app.newItemPage.clickOkButton();
+		await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName);
+	});
 
-    await app.configureFreestylePage.header.clickHome();
+	test("TC_01.001.38 | Verify Pipeline Creation", async ({ app, createJob }) => {
+		await createJob(newItemPageData.itemName, newItemPageData.itemTypes.pipeline, true);
 
-    await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName);
-  });
+		await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName);
+	});
 
-   test('TC_01.001.38 | Verify Pipeline Creation', async ({ app }: { app: App }) => {
-    await app.homePage.clickNewItemLink();
+	test(`RF_16.008.01 |  Verify deleting project via dropdown menu`, async ({ app, createJob }) => {
+		await createJob(newItemPageData.itemName, newItemPageData.itemTypes.freestyleProject, true);
 
-    await app.newItemPage.fillItemNameField(newItemPageData.itemName);
-    await app.newItemPage.clickPipeline();
-    await app.newItemPage.clickOkButton();    
-    await app.configurePipelinePage.header.clickHome();   
-    await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName);
-  });
+		await app.homePage.hoverItemName();
+		await app.homePage.openItemDropdownMenu();
+		await app.homePage.clickDeleteProjectInDropdown(newItemPageData.itemName);
+		await app.homePage.clickConfirmDeleteBtn();
+		await expect(app.homePage.projectTableRow(newItemPageData.itemName)).toHaveCount(0);
+	});
+
+	test(`RF_16.008.02 |  Verify deleting freestyle project on project's page`, async ({ app, createJob }) => {
+		await createJob(newItemPageData.itemName, newItemPageData.itemTypes.freestyleProject, true);
+
+		await app.homePage.clickItemNameLink();
+		await app.freeStyleProjectPage.clickDeleteProjectBtn();
+		await app.freeStyleProjectPage.clickYesInDeleteDialog();
+		await expect(app.homePage.projectTableRow(newItemPageData.itemName)).toHaveCount(0);
+	});
 });
 
-test.describe('US_16.008 | Freestyle Project Management > Delete Project', () => {
-  test.beforeEach(async ({ app }: { app: App }) => {
-    await app.homePage.clickNewItemLink();
-    await app.newItemPage.fillItemNameField(newItemPageData.itemName);
-    await app.newItemPage.clickFreestyleProject();
-    await app.newItemPage.clickOkButton();
-    await app.configureFreestylePage.header.clickHome();
-  });
-  test(`RF_16.008.01 |  Verify deleting project via dropdown menu`, async ({
-                                                                             app,
-                                                                           }: {
-    app: App;
-  }) => {
-    await app.homePage.hoverItemName();
-    await app.homePage.openItemDropdownMenu();
-    await app.homePage.clickDeleteProjectInDropdown(newItemPageData.itemName);
-    await app.homePage.clickConfirmDeleteBtn();
-    await expect(app.homePage.projectTableRow(newItemPageData.itemName)).toHaveCount(0);
-  });
+test.describe("US_15.001 | Footer > Jenkins version", () => {
+	test(`RF_15.001.01 | Verify Footer Version`, async ({ app }: { app: App }) => {
+		await expect(app.homePage.footer.jenkinsVersionButton()).toContainText(footer.jenkinsVersion);
+	});
 
-  test(`RF_16.008.02 |  Verify deleting freestyle project on project's page`, async ({
-                                                                                       app,
-                                                                                     }: {
-    app: App;
-  }) => {
-    await app.homePage.clickItemNameLink();
-    await app.freeStyleProjectPage.clickDeleteProjectBtn();
-    await app.freeStyleProjectPage.clickYesInDeleteDialog();
-    await expect(app.homePage.projectTableRow(newItemPageData.itemName)).toHaveCount(0);
-  });
-});
+	test(`RF_15.001.02 | Verify Footer Dropdown`, async ({ app }: { app: App }) => {
+		await app.homePage.footer.clickJenkinsVersionButton();
+		await expect(app.homePage.footer.jenkinsVersionDropdown()).toBeVisible();
+	});
 
-test.describe('US_15.001 | Footer > Jenkins version',() => {
-  test(`RF_15.001.01 | Verify Footer Version`, async ({ app } : { app : App }) => {
-    await expect(app.homePage.footer.jenkinsVersionButton()).toContainText(footer.jenkinsVersion);
-  });
+	test("RF_15.001.03 | Verify Footer Dropdown Labels", async ({ app }: { app: App }) => {
+		await app.homePage.footer.clickJenkinsVersionButton();
+		await expect(app.homePage.footer.jenkinsVersionDropdownItems()).toHaveText([
+			footer.dropdownItems.aboutJenkins.text,
+			footer.dropdownItems.getInvolved.text,
+			footer.dropdownItems.website.text
+		]);
+	});
 
-  test(`RF_15.001.02 | Verify Footer Dropdown`, async ({ app } : { app : App }) => {
-    await app.homePage.footer.clickJenkinsVersionButton();
-    await expect(app.homePage.footer.jenkinsVersionDropdown()).toBeVisible();
-  });
-  
-  test('RF_15.001.03 | Verify Footer Dropdown Labels', async ({ app } : { app: App }) => {
-    await app.homePage.footer.clickJenkinsVersionButton();
-    await expect(app.homePage.footer.jenkinsVersionDropdownItems()).toHaveText([
-      footer.dropdownItems.aboutJenkins.text,
-      footer.dropdownItems.getInvolved.text,
-      footer.dropdownItems.website.text,
-    ]);
-  });
-  
-  test('RF_15.001.04 | Verify Footer Dropdown About URL', async ({ app } : { app: App }) => {
-    await app.homePage.footer.clickJenkinsVersionButton();
-    await expect(app.homePage.footer.jenkinsVersionDropdownAbout()).toHaveAttribute('href', footer.dropdownItems.aboutJenkins.href);  
-  });
+	test("RF_15.001.04 | Verify Footer Dropdown About URL", async ({ app }: { app: App }) => {
+		await app.homePage.footer.clickJenkinsVersionButton();
+		await expect(app.homePage.footer.jenkinsVersionDropdownAbout()).toHaveAttribute(
+			"href",
+			footer.dropdownItems.aboutJenkins.href
+		);
+	});
 
-  test('RF_15.001.05 | Verify Footer Dropdown Get Involved URL', async ({ app } : { app: App }) => {
-    await app.homePage.footer.clickJenkinsVersionButton();
-    await expect(app.homePage.footer.jenkinsVersionDropdownGetInvolved()).toHaveAttribute('href', footer.dropdownItems.getInvolved.href);    
-  });
+	test("RF_15.001.05 | Verify Footer Dropdown Get Involved URL", async ({ app }: { app: App }) => {
+		await app.homePage.footer.clickJenkinsVersionButton();
+		await expect(app.homePage.footer.jenkinsVersionDropdownGetInvolved()).toHaveAttribute(
+			"href",
+			footer.dropdownItems.getInvolved.href
+		);
+	});
 });
 
 test.describe("US_01.005 | New Item > Create Multibranch Pipeline", () => {
-    test("RF_01.005.01 | Verify Multibranch Pipeline Creation", async ( {app} : {app : App}) => {
-      await app.homePage.clickNewItemLink();
-      await app.newItemPage.fillItemNameField(newItemPageData.itemName);
-      await app.newItemPage.clickMultibranchPipeline();
-      await app.newItemPage.clickOkButton();
+	test("RF_01.005.01 | Verify Multibranch Pipeline Creation", async ({ app }: { app: App }) => {
+		await app.homePage.clickNewItemLink();
 
-      await app.configureMultibranchPipelinePage.header.clickHome();
-      await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName)
-    });
-  });
+		// await app.newItemPage
+		// 	.fillItemNameField(newItemPageData.itemName)
+		// 	.then(async (o) => await o.clickMultibranchPipeline())
+		// 	.then(async (o) => await o.clickOkButton());
+
+		await app.newItemPage.fillItemNameField(newItemPageData.itemName);
+		await app.newItemPage.clickMultibranchPipeline();
+		await app.newItemPage.clickOkButton();
+
+		await app.configureMultibranchPipelinePage.header.clickHome();
+
+		await expect(app.homePage.itemName()).toHaveText(newItemPageData.itemName);
+	});
+});
+
+// Accessibility Tests (тесты доступности страниц). Для информации.
+// Эти тесты можно писать и тренироваться только локально, не Пушаем в репозиторий!
+// Каждая страница имеет свои тесты доступности
+test.describe("HomePage Accessibility Tests", { tag: "@a11y" }, () => {
+	test("acc1", async ({ app }) => {
+		const violations = await app.checkAccessibility();
+
+		expect(violations).toEqual([]);
+	});
+
+	test("acc2", async ({ app }) => {
+		const violations = await app.checkAccessibility();
+
+		const highImpactViolations = violations.filter((v) => {
+			return v.impact === "critical" || v.impact === "serious";
+		});
+
+		expect(highImpactViolations).toEqual([]);
+	});
+
+	test("acc3", async ({ app }) => {
+		const violations = await app.checkAccessibility();
+
+		expect(violations.length).toBeLessThanOrEqual(10);
+	});
+
+	test("acc4", async ({ app }) => {
+		const violations = await app.checkAccessibility();
+
+		const altTextFails = violations.filter((v) => v.id == "image-alt");
+
+		expect(altTextFails).toEqual([]);
+	});
+});
