b8dd610326de678f762543c8c882e9d02ca1e4bf
himym1989 <himym1989@gmail.com>
2026-05-19 15:02:05 +0300

    RF_16.008 | Tests moved to another folder

---
+++ POM/pageObjects/pages/StatusFreestyleProjectPage.ts
@@ -0,0 +1,21 @@
+import { BasePage } from "./@components";
+
+export class StatusFreestyleProjectPage extends BasePage {
+    // locators
+    deleteProjectBtn = () => this.page.locator('a[data-title="Delete Project"]');
+    confirmDeleteBtn = () => this.page.locator("button[data-id=ok]");
+
+    disabledProjectWarning = () => this.page.locator('form[id="enable-project"]');
+    enableProjectBtn = () => this.page.locator('button[value="Enable"]');
+    activeNavLink = () => this.page.locator("a.task-link--active");
+
+    // actions
+    async clickDeleteProjectBtn() {
+        await this.deleteProjectBtn().click();
+        return this;
+    }
+
+    async clickYesInDeleteDialog() {
+        await this.confirmDeleteBtn().click();
+    }
+}
---
+++ POM/tests/StatusFreestyleProject.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect, App } from "@/POM/fixtures/newItemPageFixtures";
+import { newItemPageData } from "../testData/newItemPageData";
+import { tr } from "@faker-js/faker";
+
+test.describe("US_02.001 | Freestyle Project Configuration > Enable or Disable the Project", async () => {
+    test.beforeEach(async ({ app, createJob }) => {
+        await createJob(newItemPageData.itemName, newItemPageData.itemTypes.freestyleProject, true);
+        await app.homePage.hoverItemName();
+        await app.homePage.openItemDropdownMenu();
+        await app.homePage.clickItemDropDownConfigureButton(newItemPageData.itemName);
+        await app.configureFreestylePage.disableProject();
+        await app.configureFreestylePage.saveChanges();
+    });
+
+    test("RF_02.001.01 |Verify that warning message appears after disabling the project", async ({
+        app,
+    }) => {
+        await expect(app.statusFreestyleProjectPage.disabledProjectWarning()).toBeVisible();
+        await expect(app.statusFreestyleProjectPage.disabledProjectWarning()).toContainText(
+            "This project is currently disabled",
+        );
+    });
+
+    test("TC_02.001.09 | Verify Enable button is shown while project is disabled", async ({
+        app,
+        createJob,
+    }) => {
+        await expect(app.statusFreestyleProjectPage.enableProjectBtn()).toBeVisible();
+        await expect(app.statusFreestyleProjectPage.enableProjectBtn()).toBeEnabled();
+    });
+
+    test("TC_02.001.10  | Verify user is redirected to project's Status page after disabling the project", async ({
+        app,
+        createJob,
+    }) => {
+        await expect(app.statusFreestyleProjectPage.activeNavLink()).toContainText("Status");
+    });
+});
