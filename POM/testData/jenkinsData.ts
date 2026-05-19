0f72d2fb063fbe9421282172ed2bb68e03d79c92
himym1989 <himym1989@gmail.com>
2026-05-19 15:39:49 +0300

    TC_11.003.04 | Welcome Dashboard > Manage Jenkins > Verify Security group contains all expected options

--- POM/testData/jenkinsData.ts
+++ POM/testData/jenkinsData.ts
@@ -1,65 +1,83 @@
 import { url } from "node:inspector";
 
 export const footer = {
-  jenkinsVersion: "Jenkins 2.541.3",
-  dropdownItems: {
-    aboutJenkins: {
-      text: "About Jenkins",
-      href: "/manage/about",
+    jenkinsVersion: "Jenkins 2.541.3",
+    dropdownItems: {
+        aboutJenkins: {
+            text: "About Jenkins",
+            href: "/manage/about",
+        },
+        getInvolved: {
+            text: "Get involved",
+            href: "https://www.jenkins.io/participate/",
+        },
+        website: {
+            text: "Website",
+            href: "https://www.jenkins.io/",
+        },
     },
-    getInvolved: {
-      text: "Get involved",
-      href: "https://www.jenkins.io/participate/",
-    },
-    website: {
-      text: "Website",
-      href: "https://www.jenkins.io/",
-    },
-  },
 };
 
 export const manageJenkinsPageData = {
-  sections: {
-    systemConfiguration: {
-      name: "System Configuration",
-      configurationItems: {
-        system: {
-          name: "System",
-          href: "configure",
+    sections: {
+        systemConfiguration: {
+            name: "System Configuration",
+            configurationItems: {
+                system: {
+                    name: "System",
+                    href: "configure",
+                },
+                tools: {
+                    name: "Tools",
+                    href: "configureTools",
+                },
+                plugins: {
+                    name: "Plugins",
+                    href: "pluginManager",
+                },
+                nodes: {
+                    name: "Nodes",
+                    href: "computer",
+                },
+                clouds: {
+                    name: "Clouds",
+                    href: "cloud",
+                },
+                appearance: {
+                    name: "Appearance",
+                    href: "appearance",
+                },
+            },
         },
-        tools: {
-          name: "Tools",
-          href: "configureTools",
+        security: {
+            name: "Security",
+            configurationItems: {
+                security: {
+                    name: "Security",
+                    href: "configureSecurity",
+                },
+                credentials: {
+                    name: "Credentials",
+                    href: "credentials",
+                },
+                credentialProviders: {
+                    name: "Credential Providers",
+                    href: "configureCredentials",
+                },
+                users: {
+                    name: "Users",
+                    href: "securityRealm",
+                },
+            },
         },
-        plugins: {
-          name: "Plugins",
-          href: "pluginManager",
+        statusInfo: {
+            name: "Status Information",
         },
-        nodes: {
-          name: "Nodes",
-          href: "computer",
+        troubleshooting: {
+            name: "Troubleshooting",
         },
-        clouds: {
-          name: "Clouds",
-          href: "cloud",
+        toolsAndActions: {
+            name: "Tools and Actions",
         },
-        appearance: {
-          name: "Appearance",
-          href: "appearance",
-        },
-      },
     },
-    security: {
-      name: "Security",
-    },
-    statusInfo: {
-      name: "Status Information",
-    },
-    troubleshooting: {
-      name: "Troubleshooting",
-    },
-    toolsAndActions: {
-      name: "Tools and Actions",
-    },
-  },
 };
