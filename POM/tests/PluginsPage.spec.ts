f9ff912c8e291ce33e09b5f9d165266b0e1d378e
Daria Katargina <dd.shuboderova@gmail.com>
2026-05-20 09:58:20 +0200

    RF_10.006.01 | Verify Available plugins are displa yed

--- POM/tests/PluginsPage.spec.ts
+++ POM/tests/PluginsPage.spec.ts
@@ -6,10 +6,10 @@ test.describe("US_10.006 | Manage Jenkins > Plugins", () => {
     test("RF_10.006.01 | Verify Available plugins are displayed", async ({ app }: { app: App }) => {
         await app.homePage.header.clickManageJenkins();
         await app.manageJenkinsPage.clickPlugins();
-        await app.pluginsPage.clickAvailableplugins()
+        await app.pluginManagerPage.clickAvailableplugins()
  
-        await app.pluginsPage.pluginsTableContent().first().waitFor({state: 'visible'})
-        const count = await app.pluginsPage.pluginsTableContent().count();
+        await app.pluginManagerPage.pluginsTableContent().first().waitFor({state: 'visible'})
+        const count = await app.pluginManagerPage.pluginsTableContent().count();
         await expect(count).toBeGreaterThan(0)
         
     })
