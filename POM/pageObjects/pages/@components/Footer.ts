acf8e7da84f5a93c890f202b6a3689f82d0e8de4
Boris Antonov <56833148+mYpka2@users.noreply.github.com>
2026-05-21 18:08:27 +0200

    RF_15.001.06-Verify-footer-dropdown-get-website-url (#637)

--- POM/pageObjects/pages/@components/Footer.ts
+++ POM/pageObjects/pages/@components/Footer.ts
@@ -6,9 +6,9 @@ export class Footer {
 	jenkinsVersionButton = () => this.page.locator("button.jenkins_ver");
 	jenkinsVersionDropdown = () => this.page.locator(".jenkins-dropdown");
 	jenkinsVersionDropdownItems = () => this.page.locator(".jenkins-dropdown__item");
-	jenkinsVersionDropdownAbout = () => this.page.getByText("About Jenkins");
-	jenkinsVersionDropdownGetInvolved = () => this.page.getByText("Get involved");
-	jenkinsVersionDropdownWebsite = () => this.page.locator("Website");
+	jenkinsVersionDropdownAbout = () => this.page.getByRole("link", { name: "About Jenkins" });
+	jenkinsVersionDropdownGetInvolved = () => this.page.getByRole("link", { name: "Get involved" });
+	jenkinsVersionDropdownWebsite = () => this.page.getByRole("link", { name: "Website" });
 
 	async clickJenkinsVersionButton() {
 		return await this.jenkinsVersionButton().click();
