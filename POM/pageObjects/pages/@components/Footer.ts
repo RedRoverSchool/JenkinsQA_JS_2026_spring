654c646f61ee88e4e7c09d0da965dd23831575d5
Boris Antonov <mypka69@gmail.com>
2026-05-21 18:03:46 +0200

    RF_15.001.06-Verify-footer-dropdown-get-website-url

--- POM/pageObjects/pages/@components/Footer.ts
+++ POM/pageObjects/pages/@components/Footer.ts
@@ -6,11 +6,8 @@ export class Footer {
 	jenkinsVersionButton = () => this.page.locator("button.jenkins_ver");
 	jenkinsVersionDropdown = () => this.page.locator(".jenkins-dropdown");
 	jenkinsVersionDropdownItems = () => this.page.locator(".jenkins-dropdown__item");
-	// jenkinsVersionDropdownAbout = () => this.page.getByText("About Jenkins");
 	jenkinsVersionDropdownAbout = () => this.page.getByRole("link", { name: "About Jenkins" });
-	// jenkinsVersionDropdownGetInvolved = () => this.page.getByText("Get involved");
-	jenkinsVersionDropdownGetInvolved = () => this.page.getByRole("link", { name: "Website" });
-	// jenkinsVersionDropdownWebsite = () => this.page.locator("Website");
+	jenkinsVersionDropdownGetInvolved = () => this.page.getByRole("link", { name: "Get involved" });
 	jenkinsVersionDropdownWebsite = () => this.page.getByRole("link", { name: "Website" });
 
 	async clickJenkinsVersionButton() {
